3821b0fe8d7da8859a7f9b9be554c8620175a118
QUENTIN453 <quentinlamauve@gmail.com>
2023-04-27 16:52:09 +0200

    change mod name

--- src/main/java/fr/iamacat/multithreading/utils/Reference.java
+++ src/main/java/fr/iamacat/multithreading/utils/Reference.java
@@ -1,8 +1,8 @@
 package fr.iamacat.multithreading.utils;
 
 public class Reference {
-    public static final String MOD_ID = "multithreaded";
-    public static final String MOD_NAME = "Multithreaded Mod";
+    public static final String MOD_ID = "multithreadingandtweaks";
+    public static final String MOD_NAME = "Multithreading and Tweaks Mod";
     public static final String MOD_VERSION = "0.1";
     public static final String MC_VERSION = "1.7.10";
     public static final String CLIENT_PROXY = "fr.iamacat.multithreading.proxy.ClientProxy";
