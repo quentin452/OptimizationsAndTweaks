bee0f7309579ab2e4bba2fe18b1cc19be96d56bd
QUENTIN453 <quentinlamauve@gmail.com>
2023-04-29 15:32:17 +0200

    Update MixinEntityAITask.java

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntityAITask.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntityAITask.java
@@ -1,3 +1,21 @@
+/*
+ * FalseTweaks
+ * Copyright (C) 2022 FalsePattern
+ * All Rights Reserved
+ * The above copyright notice, this permission notice and the word "SNEED"
+ * shall be included in all copies or substantial portions of the Software.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package fr.iamacat.multithreading.mixins.common.core;
 
 import java.util.Iterator;
@@ -25,12 +43,13 @@ import fr.iamacat.multithreading.config.MultithreadingandtweaksConfig;
 @Mixin(value = WorldServer.class, priority = 999)
 public abstract class MixinEntityAITask {
 
-    private int availableProcessors;
-    private ThreadPoolExecutor executorService;
     private int maxPoolSize;
+    private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime()
+        .availableProcessors();
+    private ThreadPoolExecutor executorService;
 
     // Store the entities to be updated in a thread-safe map
-    private ConcurrentHashMap<Integer, Entity> entitiestoAIupdate = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, Entity> entitiestoAIupdate = new ConcurrentHashMap<>();
 
     // Define a getter method for the world object
     public WorldServer getWorldServer() {
@@ -39,57 +58,56 @@ public abstract class MixinEntityAITask {
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(CallbackInfo ci) {
-        availableProcessors = Runtime.getRuntime()
-            .availableProcessors();
-        maxPoolSize = Math.max(availableProcessors * 2, 1);
+        maxPoolSize = Math.max(AVAILABLE_PROCESSORS * 2, 1);
         ThreadFactoryBuilder builder = new ThreadFactoryBuilder();
         builder.setNameFormat("Mob-Spawner-" + this.hashCode() + "-%d");
         executorService = new ThreadPoolExecutor(
-            0,
+            maxPoolSize, // fix the maximumPoolSize bug
             maxPoolSize,
             60L,
             TimeUnit.SECONDS,
             new LinkedBlockingQueue<>(), // use a LinkedBlockingQueue instead of a SynchronousQueue
             builder.build(),
-            new ThreadPoolExecutor.AbortPolicy());
+            new ThreadPoolExecutor.DiscardOldestPolicy()); // use DiscardOldestPolicy to handle rejected tasks
         executorService.allowCoreThreadTimeOut(true);
-        executorService.setThreadFactory(builder.build());
-        executorService.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
         if (!MultithreadingandtweaksConfig.enableMixinEntityAITask) {
             // Dynamically set the maximum pool size
-            int newMaxPoolSize = Math.max(availableProcessors, 1);
+            int newMaxPoolSize = Math.max(
+                Runtime.getRuntime()
+                    .availableProcessors(),
+                1);
             if (newMaxPoolSize != maxPoolSize) {
                 maxPoolSize = newMaxPoolSize;
                 executorService.setMaximumPoolSize(maxPoolSize);
             }
         }
-
-        // Update all entities in the map
-        for (Entity entity : entitiestoAIupdate.values()) {
-            entity.onEntityUpdate();
-        }
-        // Clear the map of entities to be updated for the next tick
-        entitiestoAIupdate.clear();
     }
 
     public void updateEntities() {
         // Update all entities in the map
-        for (Entity entity : entitiestoAIupdate.values()) {
-            entity.onEntityUpdate();
-        }
+        synchronized (entitiestoAIupdate) { // synchronize the map access
+            for (Entity entity : entitiestoAIupdate.values()) {
+                entity.onEntityUpdate();
+            }
 
-        // Remove dead entities from the map
-        for (Iterator<Map.Entry<Integer, Entity>> it = entitiestoAIupdate.entrySet()
-            .iterator(); it.hasNext();) {
-            Map.Entry<Integer, Entity> entry = it.next();
-            if (entry.getValue().isDead) {
-                it.remove();
+            // Remove dead entities from the map
+            for (Iterator<Map.Entry<Integer, Entity>> it = entitiestoAIupdate.entrySet()
+                .iterator(); it.hasNext();) {
+                Map.Entry<Integer, Entity> entry = it.next();
+                if (entry.getValue().isDead) {
+                    it.remove();
+                }
             }
         }
+    }
+
+    @Inject(method = "stopServer", at = @At("RETURN"))
+    private void onStopServer(CallbackInfo ci) {
+        executorService.shutdown();
     }
 
     @Inject(method = "updateEntity", at = @At("HEAD"))
@@ -122,13 +140,13 @@ public abstract class MixinEntityAITask {
                 }
             }
 
-            // Add the entity to the map to be updated later
+            // Add the entity to the thread-safe map to be updated later
             entitiestoAIupdate.putIfAbsent(entity.getEntityId(), entity);
+        }
 
-            // Add a check to remove the entity from the queue if it is dead
-            if (entity.isDead) {
-                entitiestoAIupdate.remove(entity);
-            }
+        // Remove the entity from the map if it is dead
+        if (entity.isDead) {
+            entitiestoAIupdate.remove(entity.getEntityId());
         }
     }
 }
