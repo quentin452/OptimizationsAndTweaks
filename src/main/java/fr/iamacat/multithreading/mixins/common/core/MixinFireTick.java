927001eb319e314b19047f98e85620a7925c282f
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-05-02 14:20:21 +0200

    fix crash caused by MixinFireTick

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinFireTick.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinFireTick.java
@@ -6,7 +6,9 @@ import java.util.Random;
 import java.util.concurrent.*;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockFire;
+import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,96 +31,77 @@ public abstract class MixinFireTick {
         new SynchronousQueue<>(),
         new ThreadFactoryBuilder().setNameFormat("Fire-Tick-%d").build());
 
-    private int numberOfCPUs = MultithreadingandtweaksMultithreadingConfig.numberofcpus;
-    private int batchSize = MultithreadingandtweaksMultithreadingConfig.batchsize;; // batch size
-
     private boolean isReplaceable(BlockFire block, World world, BlockPos pos) {
         return block.isReplaceable(world, pos.getX(), pos.getY(), pos.getZ());
     }
 
-    // not sure
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(CallbackInfo ci) {
     }
-
     @Unique
     private final ConcurrentLinkedQueue<int[][]> blocksToUpdate = new ConcurrentLinkedQueue<>();
-
     @Inject(method = "updateTick", at = @At("HEAD"))
     public void updateTick(World world, int x, int y, int z, Random random, CallbackInfo info) {
-        if (MultithreadingandtweaksMultithreadingConfig.enableMixinFireTick) {
-            // Process fire ticks using breadth-first search
-            boolean[][][] visited = new boolean[3][3][3]; // 3x3x3 grid centered at (x, y, z)
-            visited[1][1][1] = true;
-            ConcurrentLinkedQueue<int[][]> blocksToUpdate = this.blocksToUpdate;
-            for (int i = 0; i < numberOfCPUs; i++) {
-                blocksToUpdate.add(new int[batchSize][3]);
-            }
-            blocksToUpdate.add(new int[][] { { x, y, z } });
-            int index = 0;
-            while (index < blocksToUpdate.size()) {
-                int[][] blockCoords = blocksToUpdate.poll();
-                for (int i = 0; i < blockCoords.length; i++) {
-                    int[] coords = blockCoords[i];
-                    for (int dx = -1; dx <= 1; dx++) {
-                        for (int dy = -1; dy <= 1; dy++) {
-                            for (int dz = -1; dz <= 1; dz++) {
-                                if (dx == 0 && dy == 0 && dz == 0) {
-                                    continue; // skip the center block
-                                }
-                                int fx = coords[0] + dx;
-                                int fy = coords[1] + dy;
-                                int fz = coords[2] + dz;
-                                if (visited[dx + 1][dy + 1][dz + 1] || !world.getBlock(fx, fy, fz)
-                                    .getMaterial()
-                                    .isReplaceable()) {
-                                    continue; // skip already visited or non-replaceable blocks
-                                }
-                                visited[dx + 1][dy + 1][dz + 1] = true; // mark block as visited
-                                if (world.getBlock(fx, fy, fz) instanceof BlockFire) {
-                                    int[][] lastBatch = blocksToUpdate.peek();
-                                    if (lastBatch[batchSize - 1] != null) {
-                                        blocksToUpdate.add(new int[batchSize][3]); // create new batch
-                                        lastBatch = blocksToUpdate.peek();
-                                    }
-                                    int lastIndex = 0;
-                                    while (lastBatch[lastIndex] != null) {
-                                        lastIndex++;
-                                    }
-                                    lastBatch[lastIndex] = new int[] { fx, fy, fz }; // add adjacent fire block to
-                                                                                     // update list
-                                }
-                            }
+        if (!MultithreadingandtweaksMultithreadingConfig.enableMixinFireTick) {
+            return;
+        }
+
+        // Process fire ticks using breadth-first search
+        boolean[][][] visited = new boolean[3][3][3]; // 3x3x3 grid centered at (x, y, z)
+        visited[1][1][1] = true;
+        BlockingQueue<int[]> blocksToUpdate = new LinkedBlockingQueue<>();
+        blocksToUpdate.add(new int[]{x, y, z});
+
+        ExecutorService executor = Executors.newFixedThreadPool(MultithreadingandtweaksMultithreadingConfig.numberofcpus);
+        List<Future<Void>> futures = new ArrayList<>();
+
+        while (!blocksToUpdate.isEmpty()) {
+            int[] coords = blocksToUpdate.poll();
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    for (int dz = -1; dz <= 1; dz++) {
+                        if (dx == 0 && dy == 0 && dz == 0) {
+                            continue; // skip the center block
+                        }
+                        int fx = coords[0] + dx;
+                        int fy = coords[1] + dy;
+                        int fz = coords[2] + dz;
+
+                        Block block = world.getBlock(fx, fy, fz);
+                        if (block == null) {
+                            continue;
+                        }
+                        Material material = block.getMaterial();
+                        if (material == null || !material.isReplaceable()) {
+                            continue; // skip non-replaceable blocks
+                        }
+                        if (visited[dx + 1][dy + 1][dz + 1]) {
+                            continue; // skip already visited blocks
+                        }
+                        visited[dx + 1][dy + 1][dz + 1] = true; // mark block as visited
+                        if (world.getBlock(fx, fy, fz) instanceof BlockFire) {
+                            blocksToUpdate.add(new int[]{fx, fy, fz}); // add adjacent fire block to update list
                         }
                     }
                 }
             }
-            // Process batches in parallel using multiple threads
-            ExecutorService executor = Executors.newFixedThreadPool(numberOfCPUs);
-            List<Future<Void>> futures = new ArrayList<>();
-            for (int[][] batch : blocksToUpdate) {
-                if (batch[0] == null) {
-                    break; // reached end of batches
-                }
+
+            int[] blockToUpdate = blocksToUpdate.poll();
+            if (blockToUpdate != null) {
                 futures.add(executor.submit(() -> {
-                    for (int[] coords : batch) {
-                        if (coords == null) {
-                            break; // reached end of batch
-                        }
-                        // Code to update block at coords goes here
-                    }
+                    // Code to update block at coords goes here
                     return null;
                 }));
             }
-            for (Future<Void> future : futures) {
-                try {
-                    future.get(); // Wait for all tasks to complete
-                } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-            executorService
-                .shutdown();
         }
+
+        for (Future<Void> future : futures) {
+            try {
+                future.get(); // Wait for all tasks to complete
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
     }
 }
