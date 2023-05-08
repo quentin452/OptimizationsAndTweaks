6a93be6f7122727744944d9cf75cb5ed39033a47
QUENTIN453 <quentinlamauve@gmail.com>
2023-05-08 12:23:52 +0200

    Update MixinEntityUpdate.java

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntityUpdate.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntityUpdate.java
@@ -30,9 +30,16 @@ public abstract class MixinEntityUpdate {
     private IChunkProvider chunkProvider;
     private static final int MAX_ENTITIES_PER_TICK = MultithreadingandtweaksMultithreadingConfig.batchsize;
     private static final int CHUNK_BATCH_SIZE = MultithreadingandtweaksMultithreadingConfig.batchsize;
-    private final List<Entity> entitiesToUpdate = new LinkedList<>();
-    private final ExecutorService updateExecutor = Executors.newFixedThreadPool(MultithreadingandtweaksMultithreadingConfig.numberofcpus);
-    private Random rand = new Random();
+    private final List<Entity> entitiesToUpdate = new ArrayList<>();
+    private final ExecutorService updateExecutor = new ThreadPoolExecutor(
+        MultithreadingandtweaksMultithreadingConfig.numberofcpus,
+        MultithreadingandtweaksMultithreadingConfig.numberofcpus,
+        0L,
+        TimeUnit.MILLISECONDS,
+        new LinkedBlockingQueue<>(MAX_ENTITIES_PER_TICK)
+    );
+    private final Map<Chunk, List<EntityLiving>> entityLivingMap = new ConcurrentHashMap<>();
+    private final Set<Entity> entitiesInWater = Collections.newSetFromMap(new ConcurrentHashMap<>());
     private World world;
 
     public MixinEntityUpdate(World world, IChunkProvider chunkProvider) {
@@ -43,19 +50,74 @@ public abstract class MixinEntityUpdate {
     @Inject(method = "updateEntities", at = @At("HEAD"))
     private void onUpdateEntities(CallbackInfo ci) {
         if (MultithreadingandtweaksMultithreadingConfig.enableMixinEntityUpdate) {
-            List<Entity> entitiesToUpdateCopy = new ArrayList<>(entitiesToUpdate);
-            int numEntitiesToUpdate = Math.min(entitiesToUpdateCopy.size(), MAX_ENTITIES_PER_TICK);
-            List<Future<?>> futures = new ArrayList<>(numEntitiesToUpdate);
-            for (int i = 0; i < numEntitiesToUpdate; i++) {
-                Entity entity = entitiesToUpdateCopy.get(i);
+            entitiesInWater.clear();
+            entityLivingMap.clear();
+            for (Entity entity : entitiesToUpdate) {
+                entity.isInWater();
+                if (entity.isEntityAlive()) {
+                    entity.onEntityUpdate();
+                    if (entity instanceof EntityLivingBase) {
+                        EntityLivingBase livingEntity = (EntityLivingBase) entity;
+                        livingEntity.getHealth();
+                        livingEntity.getDataWatcher().getWatchableObjectFloat(6);
+                    }
+                }
+                if (entity.isInWater()) {
+                    entitiesInWater.add(entity);
+                }
+                if (entity instanceof EntityLiving) {
+                    int posX = MathHelper.floor_double(entity.posX);
+                    int posY = MathHelper.floor_double(entity.posY);
+                    int posZ = MathHelper.floor_double(entity.posZ);
+                    Chunk chunk = entity.worldObj.getChunkFromBlockCoords(posX, posZ);
+                    entityLivingMap.computeIfAbsent(chunk, k -> new ArrayList<>()).add((EntityLiving) entity);
+                }
+            }
+            List<Future<?>> futures = new ArrayList<>(entitiesToUpdate.size());
+            for (Entity entity : entitiesToUpdate) {
                 futures.add(updateExecutor.submit(() -> {
-                    entity.isInWater();
-                    if (entity.isEntityAlive()) {
-                        entity.onEntityUpdate();
-                        if (entity instanceof EntityLivingBase) {
-                            EntityLivingBase livingEntity = (EntityLivingBase) entity;
-                            livingEntity.getHealth();
-                            livingEntity.getDataWatcher().getWatchableObjectFloat(6);
+                    if (entitiesInWater.contains(entity)) {
+                        entity.isInWater();
+                    }
+                }));
+            }
+            for (Future<?> future : futures) {
+                try {
+                    future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            int numEntitiesToUpdate = Math.min(entitiesToUpdate.size(), MAX_ENTITIES_PER_TICK);
+            futures.clear();
+            for (int i = 0; i < numEntitiesToUpdate; i++) {
+                Entity entity = entitiesToUpdate.get(i);
+                futures.add(updateExecutor.submit(() -> {
+                    if (entity instanceof EntityLivingBase) {
+                        EntityLivingBase livingEntity = (EntityLivingBase) entity;
+                        livingEntity.getHealth();
+                        livingEntity.getDataWatcher().getWatchableObjectFloat(6);
+                    }
+                }));
+            }
+            for (Future<?> future : futures) {
+                try {
+                    future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            List<Chunk> chunks = new ArrayList<>(entityLivingMap.keySet());
+            int numChunks = chunks.size();
+            futures.clear();
+            for (int i = 0; i < numChunks; i += CHUNK_BATCH_SIZE) {
+                int endIndex = Math.min(i + CHUNK_BATCH_SIZE, numChunks);
+                List<Chunk> batchChunks = chunks.subList(i, endIndex);
+                futures.add(updateExecutor.submit(() -> {
+                    for (Chunk chunk : batchChunks) {
+                        List<EntityLiving> batchEntities = entityLivingMap.get(chunk);
+                        for (EntityLiving entity : batchEntities) {
+                            chunk.addEntity(entity);
                         }
                     }
                 }));
@@ -67,44 +129,8 @@ public abstract class MixinEntityUpdate {
                     e.printStackTrace();
                 }
             }
-
-            // Multithread and batch SpawnerAnimals.findChunksForSpawning
-            ListMultimap<Chunk, EntityLiving> entityLivingMap = ArrayListMultimap.create();
-            for (Entity entity : this.entitiesToUpdate) {
-                if (entity instanceof EntityLiving) {
-                    int posX = MathHelper.floor_double(entity.posX);
-                    int posY = MathHelper.floor_double(entity.posY);
-                    int posZ = MathHelper.floor_double(entity.posZ);
-                    Chunk chunk = entity.worldObj.getChunkFromBlockCoords(posX, posZ);
-                    entityLivingMap.put(chunk, (EntityLiving) entity);
-                }
-            }
-
-            List<Chunk> chunks = new ArrayList<>(entityLivingMap.keySet());
-            int numChunks = chunks.size();
-            List<Future<?>> spawnerFutures = new ArrayList<>(numChunks / CHUNK_BATCH_SIZE + 1);
-            for (int i = 0; i < numChunks; i += CHUNK_BATCH_SIZE) {
-                int endIndex = Math.min(i + CHUNK_BATCH_SIZE, numChunks);
-                List<Chunk> batchChunks = chunks.subList(i, endIndex);
-                spawnerFutures.add(updateExecutor.submit(() -> {
-                    for (Chunk chunk : batchChunks) {
-                        List<EntityLiving> batchEntities = entityLivingMap.get(chunk);
-                        for (EntityLiving entity : batchEntities) {
-                            chunk.addEntity(entity);
-                        }
-                    }
-                }));
-            }
-            for (Future<?> future : spawnerFutures) {
-                try {
-                    future.get();
-                } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
-
 
     @Inject(method = "addEntity", at = @At("RETURN"))
     private void onAddEntity(Entity entity, CallbackInfo ci) {
