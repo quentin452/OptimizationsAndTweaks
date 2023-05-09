946a871327e4b54f09856750d29cefea7a4bb4ae
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-05-09 16:20:29 +0200

    Update MixinBlockCHunkUpdate.java

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinBlockCHunkUpdate.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinBlockCHunkUpdate.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.falsepattern.lib.compat.ChunkPos;
 import net.minecraft.block.Block;
 import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkProviderServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,50 +23,62 @@ import fr.iamacat.multithreading.config.MultithreadingandtweaksMultithreadingCon
 @Mixin(World.class)
 public abstract class MixinBlockCHunkUpdate {
 
-    private static final int BATCH_SIZE = MultithreadingandtweaksMultithreadingConfig.batchsize;
-    private static final ExecutorService THREAD_POOL = Executors
-        .newFixedThreadPool(MultithreadingandtweaksMultithreadingConfig.numberofcpus);
+    private static int batchsize = MultithreadingandtweaksMultithreadingConfig.batchsize;
+    private static final ExecutorService THREAD_POOL = Executors.newFixedThreadPool(MultithreadingandtweaksMultithreadingConfig.numberofcpus);
 
-    @Inject(method = "updateEntities", at = @At("HEAD"))
-    private void onServerTick(CallbackInfo callbackInfo) {
+    @Inject(method = "updateBlocks", at = @At("HEAD"))
+    private void onServerTick(CallbackInfo ci) {
         if (MultithreadingandtweaksMultithreadingConfig.enableMixinBlockCHunkUpdate) {
             World world = (World) (Object) this;
             List<Chunk> chunksToTick = new ArrayList<>(
                 world.getChunkProvider()
                     .getLoadedChunkCount());
-            for (int i = 0; i < chunksToTick.size(); i += BATCH_SIZE) {
+            for (int i = 0; i < chunksToTick.size(); i += batchsize) {
                 final int start = i;
-                final int end = Math.min(i + BATCH_SIZE, chunksToTick.size());
+                final int end = Math.min(i + batchsize, chunksToTick.size());
                 THREAD_POOL.execute(() -> tickChunks(world, chunksToTick.subList(start, end)));
             }
         }
     }
 
     private void tickChunks(World world, List<Chunk> chunks) {
+        int numBatches = (int) Math.ceil(chunks.size() / (double) batchsize);
+        List<List<Chunk>> batches = new ArrayList<>(numBatches);
+
+        for (int i = 0; i < numBatches; i++) {
+            int start = i * batchsize;
+            int end = Math.min(start + batchsize, chunks.size());
+            batches.add(chunks.subList(start, end));
+        }
+
+        for (List<Chunk> batch : batches) {
+            THREAD_POOL.execute(() -> tickChunkBatch(world, batch));
+        }
+
+        // Unload queued chunks after all the batches have been ticked
+        world.getChunkProvider().unloadQueuedChunks();
+    }
+
+    private void tickChunkBatch(World world, List<Chunk> chunks) {
         for (Chunk chunk : chunks) {
+            ChunkCoordIntPair chunkPos = chunk.getChunkCoordIntPair();
             chunk.setChunkModified();
-            ChunkCoordIntPair chunkCoords = chunk.getChunkCoordIntPair();
-            world.getChunkProvider()
-                .unloadQueuedChunks();
-            world.theProfiler.startSection("chunkIOWrite");
-            world.getChunkProvider()
-                .saveChunks(true, null);
-            world.theProfiler.endSection();
-            world.theProfiler.startSection("chunkIORead");
-            world.getChunkProvider()
-                .loadChunk(chunkCoords.chunkXPos, chunkCoords.chunkZPos);
-            world.theProfiler.endSection();
-            for (int x = chunk.xPosition * 16; x < chunk.xPosition * 16 + 16; x++) {
-                for (int z = chunk.zPosition * 16; z < chunk.zPosition * 16 + 16; z++) {
-                    for (int y = 0; y < world.getHeight(); y++) {
-                        Block block = world.getBlock(x, y, z);
+
+            // Iterate over every block in the chunk and update it
+            for (int x = 0; x < 16; x++) {
+                for (int y = 0; y < 256; y++) {
+                    for (int z = 0; z < 16; z++) {
+                        Block block = chunk.getBlock(x, y, z);
                         if (block != null) {
-                            block.updateTick(world, x, y, z, world.rand);
+                            block.updateTick(world, x + chunkPos.chunkXPos * 16, y, z + chunkPos.chunkZPos * 16, world.rand);
                         }
                     }
                 }
             }
+
+            // Save changes to the chunk
+            chunk.onChunkUnload();
+            ((IChunkProvider) world.getChunkProvider()).saveChunks(true, null);
         }
     }
-
 }
