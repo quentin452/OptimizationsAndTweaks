a4cd81a42d37f24ea8f6d45cdc3102a902874c7a
QUENTIN453 <quentinlamauve@gmail.com>
2023-09-09 01:21:05 +0200

    Update ChunkProviderGenerateTwo.java

--- src/main/java/fr/iamacat/multithreading/worldgen/ChunkProviderGenerateTwo.java
+++ src/main/java/fr/iamacat/multithreading/worldgen/ChunkProviderGenerateTwo.java
@@ -71,7 +71,6 @@ public class ChunkProviderGenerateTwo implements IChunkProvider {
     double[] field_147425_f;
     double[] field_147426_g;
     int[][] field_73219_j = new int[32][32];
-    private static final String __OBFID = "CL_00000396";
 
     {
         caveGenerator = TerrainGen.getModdedMapGen(caveGenerator, CAVE);
