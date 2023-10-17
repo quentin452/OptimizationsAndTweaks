5f7aeac3df0ba35b3b6c17310eb43dd1a7141744
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-10-17 08:19:21 +0200

    Update MixinEntitySquid.java

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/entity/MixinEntitySquid.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/entity/MixinEntitySquid.java
@@ -53,57 +53,70 @@ public class MixinEntitySquid extends EntityWaterMob {
      * use this to react to sunlight and start to burn.
      */
     @Overwrite
-    public void onLivingUpdate() {
+    public void onLivingUpdate()
+    {
         super.onLivingUpdate();
+        this.prevSquidPitch = this.squidPitch;
+        this.prevSquidYaw = this.squidYaw;
+        this.prevSquidRotation = this.squidRotation;
+        this.lastTentacleAngle = this.tentacleAngle;
+        this.squidRotation += this.rotationVelocity;
 
-        final float twoPI = (float) Math.PI * 2F;
-        final float quarterPI = (float) Math.PI * 0.25F;
+        if (this.squidRotation > ((float)Math.PI * 2F))
+        {
+            this.squidRotation -= ((float)Math.PI * 2F);
 
-        float motionX = this.randomMotionVecX * this.randomMotionSpeed;
-        float motionY = this.randomMotionVecY * this.randomMotionSpeed;
-        float motionZ = this.randomMotionVecZ * this.randomMotionSpeed;
+            if (this.rand.nextInt(10) == 0)
+            {
+                this.rotationVelocity = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
+            }
+        }
 
-        float f = MathHelper.sqrt_double(motionX * motionX + motionZ * motionZ);
+        if (this.isInWater())
+        {
+            float f;
 
-        float newRenderYawOffset = (-((float) Math.atan2(motionX, motionZ)) * 180.0F / (float) Math.PI - this.renderYawOffset) * 0.1F;
-        this.renderYawOffset += newRenderYawOffset;
-        this.rotationYaw = this.renderYawOffset;
+            if (this.squidRotation < (float)Math.PI)
+            {
+                f = this.squidRotation / (float)Math.PI;
+                this.tentacleAngle = MathHelper.sin(f * f * (float)Math.PI) * (float)Math.PI * 0.25F;
 
-        this.squidYaw += (float) Math.PI * this.field_70871_bB * 1.5F;
-
-        float newSquidPitch = (-((float) Math.atan2(f, motionY) * 180.0F / (float) Math.PI - this.squidPitch) * 0.1F);
-        this.squidPitch += newSquidPitch;
-
-        if (this.isInWater()) {
-            float squidRotation = this.squidRotation + this.rotationVelocity;
-
-            if (squidRotation > twoPI) {
-                squidRotation -= twoPI;
-
-                if (this.rand.nextInt(10) == 0) {
-                    this.rotationVelocity = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
+                if ((double)f > 0.75D)
+                {
+                    this.randomMotionSpeed = 1.0F;
+                    this.field_70871_bB = 1.0F;
+                }
+                else
+                {
+                    this.field_70871_bB *= 0.8F;
                 }
             }
-
-            float f2 = squidRotation / (float) Math.PI;
-            this.tentacleAngle = MathHelper.sin(f2 * f2 * (float) Math.PI) * quarterPI;
-
-            if (f2 > 0.75D) {
-                this.randomMotionSpeed = 1.0F;
-                this.field_70871_bB = 1.0F;
-            } else {
-                this.field_70871_bB *= 0.8F;
+            else
+            {
+                this.tentacleAngle = 0.0F;
+                this.randomMotionSpeed *= 0.9F;
+                this.field_70871_bB *= 0.99F;
             }
 
-            if (!this.worldObj.isRemote) {
-                this.motionX = motionX;
-                this.motionY = motionY;
-                this.motionZ = motionZ;
+            if (!this.worldObj.isRemote)
+            {
+                this.motionX = this.randomMotionVecX * this.randomMotionSpeed;
+                this.motionY = this.randomMotionVecY * this.randomMotionSpeed;
+                this.motionZ = this.randomMotionVecZ * this.randomMotionSpeed;
             }
-        } else {
-            this.tentacleAngle = MathHelper.abs(MathHelper.sin(this.squidRotation)) * quarterPI;
 
-            if (!this.worldObj.isRemote) {
+            f = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
+            this.renderYawOffset += (-((float)Math.atan2(this.motionX, this.motionZ)) * 180.0F / (float)Math.PI - this.renderYawOffset) * 0.1F;
+            this.rotationYaw = this.renderYawOffset;
+            this.squidYaw += (float)Math.PI * this.field_70871_bB * 1.5F;
+            this.squidPitch += (-((float)Math.atan2(f, this.motionY)) * 180.0F / (float)Math.PI - this.squidPitch) * 0.1F;
+        }
+        else
+        {
+            this.tentacleAngle = MathHelper.abs(MathHelper.sin(this.squidRotation)) * (float)Math.PI * 0.25F;
+
+            if (!this.worldObj.isRemote)
+            {
                 this.motionX = 0.0D;
                 this.motionY -= 0.08D;
                 this.motionY *= 0.9800000190734863D;
