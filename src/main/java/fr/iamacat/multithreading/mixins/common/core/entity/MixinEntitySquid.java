7d6e683ebe6237efb437430d9c54c598187877b8
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-10-18 16:32:31 +0200

    Add Apache FastMath 3 + Update MixinEntitySquid

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/entity/MixinEntitySquid.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/entity/MixinEntitySquid.java
@@ -1,19 +1,16 @@
 package fr.iamacat.multithreading.mixins.common.core.entity;
 
 import fr.iamacat.multithreading.config.MultithreadingandtweaksConfig;
+import fr.iamacat.multithreading.utils.apache.commons.math3.util.FastMath;
 import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.entity.passive.EntityWaterMob;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = EntitySquid.class, priority = 999)
+@Mixin(EntitySquid.class)
 public class MixinEntitySquid extends EntityWaterMob {
 
     @Shadow
@@ -54,60 +51,85 @@ public class MixinEntitySquid extends EntityWaterMob {
         super(p_i1695_1_);
     }
 
+
     /**
      * Called frequently so the entity can update its state every tick as required. For example, zombies and skeletons
      * use this to react to sunlight and start to burn.
      */
-    @Inject(method = "onLivingUpdate", at = @At("HEAD"), remap = false, cancellable = true)
-    public void modifiedonLivingUpdate(CallbackInfo ci) {
-        if (MultithreadingandtweaksConfig.enableMixinEntitySquid) {
-            super.onLivingUpdate();
-            this.prevSquidPitch = this.squidPitch;
-            this.prevSquidYaw = this.squidYaw;
-            this.prevSquidRotation = this.squidRotation;
-            this.lastTentacleAngle = this.tentacleAngle;
-            this.squidRotation += this.rotationVelocity;
+    @Overwrite
+    public void onLivingUpdate()
+    {
+        if (MultithreadingandtweaksConfig.enableMixinEntitySquid){
+        super.onLivingUpdate();
+        this.prevSquidPitch = this.squidPitch;
+        this.prevSquidYaw = this.squidYaw;
+        this.prevSquidRotation = this.squidRotation;
+        this.lastTentacleAngle = this.tentacleAngle;
+        this.squidRotation += this.rotationVelocity;
 
-            if (this.squidRotation > ((float) Math.PI * 2F)) {
-                this.squidRotation -= ((float) Math.PI * 2F);
+        if (this.squidRotation > ((float)Math.PI * 2F))
+        {
+            this.squidRotation -= ((float)Math.PI * 2F);
 
-                if (this.rand.nextInt(10) == 0) {
-                    this.rotationVelocity = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
-                }
+            if (this.rand.nextInt(10) == 0)
+            {
+                this.rotationVelocity = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
             }
         }
 
-        if (this.isInWater()) {
-            float f = 0;
+        if (this.isInWater())
+        {
+            float f;
 
-            if (this.squidRotation < (float) Math.PI) {
-                f = this.squidRotation / (float) Math.PI;
-                this.tentacleAngle = MathHelper.sin(f * f * (float) Math.PI) * (float) Math.PI * 0.25F;
+            if (this.squidRotation < (float)Math.PI)
+            {
+                f = this.squidRotation / (float)Math.PI;
+                this.tentacleAngle = MathHelper.sin(f * f * (float)Math.PI) * (float)Math.PI * 0.25F;
 
-                if ((double) f > 0.75D) {
+                if ((double)f > 0.75D)
+                {
                     this.randomMotionSpeed = 1.0F;
                     this.field_70871_bB = 1.0F;
-                } else {
+                }
+                else
+                {
                     this.field_70871_bB *= 0.8F;
                 }
-            } else {
+            }
+            else
+            {
                 this.tentacleAngle = 0.0F;
                 this.randomMotionSpeed *= 0.9F;
                 this.field_70871_bB *= 0.99F;
             }
 
-            if (!this.worldObj.isRemote) {
-                double atan2MotionX = Math.atan2(this.motionX, this.motionZ);
-                double atan2MotionY = Math.atan2(f, this.motionY);
+            if (!this.worldObj.isRemote)
+            {
                 this.motionX = this.randomMotionVecX * this.randomMotionSpeed;
                 this.motionY = this.randomMotionVecY * this.randomMotionSpeed;
                 this.motionZ = this.randomMotionVecZ * this.randomMotionSpeed;
-                this.renderYawOffset += (-((float) atan2MotionX) * 180.0F / (float) Math.PI - this.renderYawOffset) * 0.1F;
-                this.rotationYaw = this.renderYawOffset;
-                this.squidYaw += (float) Math.PI * this.field_70871_bB * 1.5F;
-                this.squidPitch += (-((float) atan2MotionY) * 180.0F / (float) Math.PI - this.squidPitch) * 0.1F;
             }
+
+            f = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
+            this.renderYawOffset += (-((float)FastMath.atan2(this.motionX, this.motionZ)) * 180.0F / (float)Math.PI - this.renderYawOffset) * 0.1F;
+            this.rotationYaw = this.renderYawOffset;
+            this.squidYaw += (float)Math.PI * this.field_70871_bB * 1.5F;
+            this.squidPitch += (-((float)FastMath.atan2(f, this.motionY)) * 180.0F / (float)Math.PI - this.squidPitch) * 0.1F;
         }
-        ci.cancel();
+        else
+        {
+            this.tentacleAngle = MathHelper.abs(MathHelper.sin(this.squidRotation)) * (float)Math.PI * 0.25F;
+
+            if (!this.worldObj.isRemote)
+            {
+                this.motionX = 0.0D;
+                this.motionY -= 0.08D;
+                this.motionY *= 0.9800000190734863D;
+                this.motionZ = 0.0D;
+            }
+
+            this.squidPitch = (float)((double)this.squidPitch + (double)(-90.0F - this.squidPitch) * 0.02D);
+        }
+    }
     }
 }
