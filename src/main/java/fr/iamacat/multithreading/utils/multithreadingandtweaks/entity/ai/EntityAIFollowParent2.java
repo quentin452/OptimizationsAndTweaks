538cac2797d36b66e26faa22a174ed5e84ae9eb3
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-10-19 03:22:28 +0200

    Update EntityAIFollowParent2.java

--- src/main/java/fr/iamacat/multithreading/utils/multithreadingandtweaks/entity/ai/EntityAIFollowParent2.java
+++ src/main/java/fr/iamacat/multithreading/utils/multithreadingandtweaks/entity/ai/EntityAIFollowParent2.java
@@ -2,6 +2,7 @@ package fr.iamacat.multithreading.utils.multithreadingandtweaks.entity.ai;
 
 import fr.iamacat.multithreading.config.MultithreadingandtweaksConfig;
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.passive.EntityAnimal;
 
 import java.util.List;
@@ -10,55 +11,18 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class EntityAIFollowParent2 extends EntityAIBase {
-    EntityAnimal childAnimal;
-    EntityAnimal parentAnimal;
-    double field_75347_c;
-    private int field_75345_d;
-    private final ExecutorService executorService;
-    private Future<?> followTask;
+    private final EntityAnimal childAnimal;
+    private final double followDistanceSq;
+    private EntityAnimal parentAnimal;
 
-    public EntityAIFollowParent2(EntityAnimal p_i1626_1_, double p_i1626_2_) {
-        this.childAnimal = p_i1626_1_;
-        this.field_75347_c = p_i1626_2_;
-        this.executorService = Executors.newFixedThreadPool(MultithreadingandtweaksConfig.numberofcpus);
+    public EntityAIFollowParent2(EntityAnimal childAnimal, double followDistance) {
+        this.childAnimal = childAnimal;
+        this.followDistanceSq = followDistance * followDistance;
     }
 
-    public boolean shouldExecute() {
-        if (!this.childAnimal.isChild()) {
-            return false;
-        } else {
-            List<EntityAnimal> list = getCachedEntitiesWithinAABB();
-            EntityAnimal entityAnimal = null;
-            double minDistance = Double.MAX_VALUE;
+    private List<EntityAnimal> cachedEntitiesWithinAABB = null;
 
-            for (EntityAnimal entityAnimal1 : list) {
-                if (!entityAnimal1.isChild()) {
-                    double dx = entityAnimal1.posX - childAnimal.posX;
-                    double dy = entityAnimal1.posY - childAnimal.posY;
-                    double dz = entityAnimal1.posZ - childAnimal.posZ;
-                    double distance = dx * dx + dy * dy + dz * dz;
-
-                    if (distance <= minDistance) {
-                        minDistance = distance;
-                        entityAnimal = entityAnimal1;
-                    }
-                }
-            }
-
-            if (entityAnimal == null) {
-                return false;
-            } else if (minDistance < 9.0D) {
-                return false;
-            } else {
-                this.parentAnimal = entityAnimal;
-                return true;
-            }
-        }
-    }
-
-    private List cachedEntitiesWithinAABB = null;
-
-    private List getCachedEntitiesWithinAABB() {
+    private List<EntityAnimal> getCachedEntitiesWithinAABB() {
         if (cachedEntitiesWithinAABB == null) {
             cachedEntitiesWithinAABB = this.childAnimal.worldObj.getEntitiesWithinAABB(
                 this.childAnimal.getClass(), this.childAnimal.boundingBox.expand(8.0D, 4.0D, 8.0D)
@@ -68,30 +32,34 @@ public class EntityAIFollowParent2 extends EntityAIBase {
     }
 
     public boolean continueExecuting() {
-        if (!this.parentAnimal.isEntityAlive()) {
-            return false;
-        } else {
-            double d0 = this.childAnimal.getDistanceSqToEntity(this.parentAnimal);
-            return d0 >= 9.0D && d0 <= 256.0D;
-        }
+        return this.parentAnimal != null && this.parentAnimal.isEntityAlive() &&
+            this.childAnimal.getDistanceSqToEntity(this.parentAnimal) >= 9.0D;
     }
 
-    public void startExecuting() {
-        this.field_75345_d = 0;
-        this.followTask = executorService.submit(() -> {
-            while (continueExecuting()) {
-                if (--this.field_75345_d <= 0) {
-                    this.field_75345_d = 10;
-                    this.childAnimal.getNavigator().tryMoveToEntityLiving(this.parentAnimal, this.field_75347_c);
+    public boolean shouldExecute() {
+        List<EntityAnimal> nearbyEntities = getCachedEntitiesWithinAABB();
+        double minDistanceSq = Double.MAX_VALUE;
+        boolean hasValidParent = false;
+
+        for (EntityAnimal entity : nearbyEntities) {
+            int growingAge = entity.getGrowingAge();
+            if (growingAge <= 0) {
+                double distanceSq = this.childAnimal.getDistanceSqToEntity(entity);
+
+                if (distanceSq <= this.followDistanceSq && distanceSq < minDistanceSq) {
+                    minDistanceSq = distanceSq;
+                    this.parentAnimal = entity;
+                    hasValidParent = true;
                 }
             }
-        });
-    }
-
-    public void resetTask() {
-        this.parentAnimal = null;
-        if (followTask != null) {
-            followTask.cancel(true);
         }
+
+        if (hasValidParent && this.childAnimal.getGrowingAge() <= 0) {
+            while (continueExecuting()) {
+                this.childAnimal.getNavigator().tryMoveToEntityLiving(this.parentAnimal, this.followDistanceSq);
+            }
+        }
+
+        return hasValidParent && this.childAnimal.getDistanceToEntity(this.parentAnimal) > 5.0;
     }
 }
