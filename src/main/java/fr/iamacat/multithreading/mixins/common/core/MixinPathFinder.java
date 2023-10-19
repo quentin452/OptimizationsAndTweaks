894a9502c9e7c86425f2a6ee9c50a2a9d4a336df
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-10-19 17:47:45 +0200

    Update MixinPathFinder + add better pathfinding

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinPathFinder.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinPathFinder.java
@@ -1,10 +1,24 @@
 package fr.iamacat.multithreading.mixins.common.core;
 
+import fr.iamacat.multithreading.config.MultithreadingandtweaksConfig;
+import fr.iamacat.multithreading.utils.multithreadingandtweaks.entity.pathfinding.PathEntity2;
+import fr.iamacat.multithreading.utils.multithreadingandtweaks.entity.pathfinding.PathPoint2;
+import net.minecraft.entity.Entity;
 import net.minecraft.pathfinding.PathFinder;
-
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PathFinder.class)
 public class MixinPathFinder {
-    // wip
+
+    @Inject(method = "addToPath", at = @At("HEAD"), remap = false, cancellable = true)
+    private PathEntity2 addToPath(Entity p_75861_1_, PathPoint2 p_75861_2_, PathPoint2 p_75861_3_, PathPoint2 p_75861_4_, float p_75861_5_, CallbackInfo ci) {
+        if (MultithreadingandtweaksConfig.enableMixinPathFinder) {
+            return addToPath(p_75861_1_, p_75861_2_, p_75861_3_, p_75861_4_, p_75861_5_,ci);
+        }
+        ci.cancel();
+        return null;
+    }
 }
