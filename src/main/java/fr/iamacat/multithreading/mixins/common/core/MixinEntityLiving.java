c00bc573a7f0a4149e777f66f9ae1b346ee42f20
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-10-20 00:07:00 +0200

    Update MixinEntityLiving.java

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntityLiving.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntityLiving.java
@@ -7,10 +7,12 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Iterator;
 
@@ -18,6 +20,8 @@ import java.util.Iterator;
 public abstract class MixinEntityLiving extends EntityLivingBase {
     @Unique
     private World p_i1595_1_;
+    @Shadow
+    private boolean canPickUpLoot;
 
     @Unique
     public EntityAITasks2 multithreadingandtweaks$tasks;
@@ -33,7 +37,7 @@ public abstract class MixinEntityLiving extends EntityLivingBase {
 
     @Inject(at = @At(value = "RETURN"), method = "<init>(Lnet/minecraft/world/World;)V")
     private void modifyTasks(World worldIn, CallbackInfo ci) {
-        if(MultithreadingandtweaksConfig.enableMixinEntityChicken){
+        if(MultithreadingandtweaksConfig.enableMixinEntityLiving){
             Iterator iterator = ((EntityLiving) (Object) this).tasks.taskEntries.iterator();
             while (iterator.hasNext()) {
                 EntityLiving entry = (EntityLiving) iterator.next();
@@ -45,5 +49,30 @@ public abstract class MixinEntityLiving extends EntityLivingBase {
             this.multithreadingandtweaks$tasks = new EntityAITasks2(p_i1595_1_ != null && p_i1595_1_.theProfiler != null ? p_i1595_1_.theProfiler : null);
             this.multithreadingandtweaks$targetTasks = new EntityAITasks2(p_i1595_1_ != null && p_i1595_1_.theProfiler != null ? p_i1595_1_.theProfiler : null);
         }
+    }
+    @Unique
+    private boolean cachedCanPickUpLoot = false;
+    @Unique
+    private long lastCheckTime = 0L;
+    @Unique
+    private static final long CACHE_EXPIRATION_TIME = 1000L;
+
+    @Inject(method = "canPickUpLoot", at = @At("HEAD"), remap = false, cancellable = true)
+    public boolean canPickUpLoot(CallbackInfo ci)
+    {
+        if (MultithreadingandtweaksConfig.enableMixinEntityLiving){
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime - lastCheckTime < CACHE_EXPIRATION_TIME) {
+            return cachedCanPickUpLoot;
+        } else {
+            boolean canPickUpLoot = this.canPickUpLoot;
+            cachedCanPickUpLoot = canPickUpLoot;
+            lastCheckTime = currentTime;
+            return canPickUpLoot;
+        }
+    }
+        ci.cancel();
+        return false;
     }
 }
