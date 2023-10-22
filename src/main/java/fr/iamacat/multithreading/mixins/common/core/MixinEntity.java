ef53d44cb8e8534cf18e521c7a210dbbef436514
QUENTIN453 <quentinlamauve@gmail.com>
2023-10-22 05:52:28 +0200

    Update MixinEntity.java

--- src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntity.java
+++ src/main/java/fr/iamacat/multithreading/mixins/common/core/MixinEntity.java
@@ -1,21 +1,29 @@
 package fr.iamacat.multithreading.mixins.common.core;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
+import net.minecraftforge.common.IExtendedEntityProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import fr.iamacat.multithreading.config.MultithreadingandtweaksConfig;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Mixin(Entity.class)
 public class MixinEntity {
@@ -78,7 +86,7 @@ public class MixinEntity {
     @Unique
     private static final long CACHE_EXPIRATION_TIME = 1000L;
 
-    @Inject(method = "isInWater", at = @At("HEAD"), remap = false, cancellable = true)
+    @Inject(method = "isInWater", at = @At("HEAD"),remap = false, cancellable = true)
     public boolean isInWater(CallbackInfo ci) {
         if (MultithreadingandtweaksConfig.enableMixinEntity) {
             long currentTime = System.currentTimeMillis();
@@ -96,4 +104,16 @@ public class MixinEntity {
             return false;
         }
     }
+
+    @Unique
+    protected ConcurrentHashMap<String, IExtendedEntityProperties> multithreadingandtweaks$extendedProperties;
+    @Redirect(
+        method = "getExtendedProperties",
+        at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;extendedProperties:Ljava/util/Map;"),
+        remap = false
+    )
+    private Map<String, IExtendedEntityProperties> redirectExtendedProperties(Entity entity) {
+        return multithreadingandtweaks$extendedProperties;
+    }
+
 }
