1916532ec44b606b26926f6eb65812f7a14a278a
QUENTIN453 <quentinlamauve@gmail.com>
2023-10-25 11:18:02 +0200

    fix a mistake

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinBlockLiquid.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinBlockLiquid.java
@@ -124,38 +124,4 @@ public class MixinBlockLiquid extends Block {
 
         return metadata >= 8 ? 0 : metadata;
     }
-
-    @Mixin(Chunk.class)
-    public static class MixinChunkEntityCollisionFix {
-        @Shadow
-        public List[] entityLists;
-        /**
-         * @author
-         * @reason
-         */
-        @Overwrite
-        public void getEntitiesWithinAABBForEntity(Entity p_76588_1_, AxisAlignedBB p_76588_2_, List<Entity> p_76588_3_,
-                                                   IEntitySelector p_76588_4_) {
-
-            if (OptimizationsandTweaksConfig.enableMixinChunk) {
-                int i = MathHelper.floor_double((p_76588_2_.minY - World.MAX_ENTITY_RADIUS) / 16.0D);
-                int j = MathHelper.floor_double((p_76588_2_.maxY + World.MAX_ENTITY_RADIUS) / 16.0D);
-                i = MathHelper.clamp_int(i, 0, this.entityLists.length - 1);
-                j = MathHelper.clamp_int(j, 0, this.entityLists.length - 1);
-
-                for (int k = i; k <= j; ++k) {
-                    List list1 = this.entityLists[k];
-
-                    for (Object o : list1) {
-                        Entity entity1 = (Entity) o;
-
-                        if (entity1 != p_76588_1_ && entity1.boundingBox.intersectsWith(p_76588_2_)
-                            && (p_76588_4_ == null || p_76588_4_.isEntityApplicable(entity1))) {
-                            p_76588_3_.add(entity1);
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
