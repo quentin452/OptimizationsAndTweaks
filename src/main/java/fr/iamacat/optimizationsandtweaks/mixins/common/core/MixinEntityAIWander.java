903b9cae9a957d4db583c2c39738cab9ab4318b9
QUENTIN453 <quentinlamauve@gmail.com>
2023-10-25 17:12:25 +0200

    Update MixinEntityAIWander.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityAIWander.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityAIWander.java
@@ -4,14 +4,16 @@ import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.RandomPositionGenerator;
+import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.util.Vec3;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(EntityAIWander.class)
-public class MixinEntityAIWander extends EntityAIBase { // WIP
+public class MixinEntityAIWander extends EntityAIBase {
 
     @Shadow
     private EntityCreature entity;
@@ -24,6 +26,9 @@ public class MixinEntityAIWander extends EntityAIBase { // WIP
     @Shadow
     private double speed;
 
+    @Unique
+    private PathEntity multithreadingandtweaks$cachedPath;
+
     public MixinEntityAIWander(EntityCreature p_i1648_1_, double p_i1648_2_) {
         this.entity = p_i1648_1_;
         this.speed = p_i1648_2_;
@@ -31,42 +36,47 @@ public class MixinEntityAIWander extends EntityAIBase { // WIP
     }
 
     /**
-     * Returns whether the EntityAIBase should begin execution.
+     * @author
+     * @reason
      */
     @Overwrite
     public boolean shouldExecute() {
         if (this.entity.getAge() >= 100) {
             return false;
-        } else if (this.entity.getRNG()
-            .nextInt(120) != 0) {
+        } else if (this.entity.getRNG().nextInt(120) != 0) {
+            return false;
+        } else {
+            if (multithreadingandtweaks$cachedPath != null) {
+                return true;
+            }
+
+            Vec3 vec3 = RandomPositionGenerator.findRandomTarget(this.entity, 10, 7);
+
+            if (vec3 == null) {
                 return false;
             } else {
-                Vec3 vec3 = RandomPositionGenerator.findRandomTarget(this.entity, 10, 7);
-
-                if (vec3 == null) {
-                    return false;
-                } else {
-                    this.xPosition = vec3.xCoord;
-                    this.yPosition = vec3.yCoord;
-                    this.zPosition = vec3.zCoord;
-                    return true;
-                }
+                this.xPosition = vec3.xCoord;
+                this.yPosition = vec3.yCoord;
+                this.zPosition = vec3.zCoord;
+                return true;
             }
+        }
     }
 
-    /**
-     * Returns whether an in-progress EntityAIBase should continue executing
-     */
     public boolean continueExecuting() {
-        return !this.entity.getNavigator()
-            .noPath();
+        return !this.entity.getNavigator().noPath();
     }
 
-    /**
-     * Execute a one shot task or start executing a continuous task
-     */
     public void startExecuting() {
-        this.entity.getNavigator()
-            .tryMoveToXYZ(this.xPosition, this.yPosition, this.zPosition, this.speed);
+        if (multithreadingandtweaks$cachedPath != null) {
+            this.entity.getNavigator().setPath(multithreadingandtweaks$cachedPath, this.speed);
+        } else {
+            PathEntity path = this.entity.getNavigator().getPathToXYZ(this.xPosition, this.yPosition, this.zPosition);
+
+            if (path != null) {
+                multithreadingandtweaks$cachedPath = path;
+                this.entity.getNavigator().setPath(path, this.speed);
+            }
+        }
     }
 }
