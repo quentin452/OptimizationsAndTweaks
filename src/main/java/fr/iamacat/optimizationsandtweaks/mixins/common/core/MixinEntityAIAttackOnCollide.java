77cddec52ae9b6609225d87bf83db6946d75c953
QUENTIN453 <quentinlamauve@gmail.com>
2023-10-25 17:46:54 +0200

    Update MixinEntityAIAttackOnCollide.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityAIAttackOnCollide.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityAIAttackOnCollide.java
@@ -5,13 +5,13 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.pathfinding.PathPoint;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(EntityAIAttackOnCollide.class)
 public class MixinEntityAIAttackOnCollide {
@@ -45,73 +45,124 @@ public class MixinEntityAIAttackOnCollide {
 
     @Shadow
     private int failedPathFindingPenalty;
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public boolean shouldExecute() {
+        EntityLivingBase targetEntity = this.attacker.getAttackTarget();
 
-    @Inject(method = "updateTask", at = @At("HEAD"), cancellable = true)
-    public void updateTask(CallbackInfo ci) {
-        EntityLivingBase entitylivingbase = this.attacker.getAttackTarget();
+        if (targetEntity == null || !targetEntity.isEntityAlive() || (this.classTarget != null && !this.classTarget.isAssignableFrom(targetEntity.getClass()))) {
+            return false;
+        }
 
-        if (entitylivingbase == null) {
+        if (--this.field_75445_i <= 0) {
+            this.entityPathEntity = this.attacker.getNavigator().getPathToEntityLiving(targetEntity);
+            this.field_75445_i = 4 + this.attacker.getRNG().nextInt(7);
+            return this.entityPathEntity != null;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public boolean continueExecuting() {
+        EntityLivingBase targetEntity = this.attacker.getAttackTarget();
+        if (targetEntity == null || !targetEntity.isEntityAlive()) {
+            return false;
+        }
+
+        if (!this.longMemory) {
+            return !this.attacker.getNavigator().noPath();
+        } else {
+            return this.attacker.isWithinHomeDistance(MathHelper.floor_double(targetEntity.posX), MathHelper.floor_double(targetEntity.posY), MathHelper.floor_double(targetEntity.posZ));
+        }
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void startExecuting() {
+        this.attacker.getNavigator().setPath(this.entityPathEntity, this.speedTowardsTarget);
+        this.field_75445_i = 0;
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void updateTask() {
+        EntityLivingBase targetEntity = this.attacker.getAttackTarget();
+
+        if (targetEntity == null) {
             return;
         }
 
-        this.attacker.getLookHelper()
-            .setLookPositionWithEntity(entitylivingbase, 30.0F, 30.0F);
+        this.attacker.getLookHelper().setLookPositionWithEntity(targetEntity, 30.0F, 30.0F);
 
-        double d0 = this.attacker
-            .getDistanceSq(entitylivingbase.posX, entitylivingbase.boundingBox.minY, entitylivingbase.posZ);
-        double d1 = (this.attacker.width * 2.0F * this.attacker.width * 2.0F + entitylivingbase.width);
+        double distanceSq = this.attacker.getDistanceSq(targetEntity.posX, targetEntity.boundingBox.minY, targetEntity.posZ);
+        double maxAttackDistance = this.attacker.width * 2.0F * this.attacker.width * 2.0F + targetEntity.width;
 
-        if (d0 <= d1 && this.attackTick <= 20) {
+        if (distanceSq <= maxAttackDistance && this.attackTick <= 20) {
             this.attackTick = 20;
-
             if (this.attacker.getHeldItem() != null) {
                 this.attacker.swingItem();
             }
-
-            this.attacker.attackEntityAsMob(entitylivingbase);
+            this.attacker.attackEntityAsMob(targetEntity);
             return;
         }
 
-        if (this.longMemory || this.attacker.getEntitySenses()
-            .canSee(entitylivingbase)) {
-            --this.field_75445_i;
-
+        if (this.longMemory || this.attacker.getEntitySenses().canSee(targetEntity)) {
             if (this.field_75445_i <= 0) {
-                this.field_151497_i = entitylivingbase.posX;
-                this.field_151495_j = entitylivingbase.boundingBox.minY;
-                this.field_151496_k = entitylivingbase.posZ;
-                this.field_75445_i = failedPathFindingPenalty + 4
-                    + this.attacker.getRNG()
-                        .nextInt(7);
+                this.multithreadingandtweaks$updatePathToTarget(targetEntity);
+            } else {
+                this.field_75445_i--;
+            }
+        }
+    }
 
-                if (this.attacker.getNavigator()
-                    .getPath() != null) {
-                    PathPoint finalPathPoint = this.attacker.getNavigator()
-                        .getPath()
-                        .getFinalPathPoint();
+    @Unique
+    private void multithreadingandtweaks$updatePathToTarget(EntityLivingBase targetEntity) {
+        this.field_151497_i = targetEntity.posX;
+        this.field_151495_j = targetEntity.boundingBox.minY;
+        this.field_151496_k = targetEntity.posZ;
 
-                    if (finalPathPoint != null && entitylivingbase
-                        .getDistanceSq(finalPathPoint.xCoord, finalPathPoint.yCoord, finalPathPoint.zCoord) < 1) {
-                        failedPathFindingPenalty = 0;
-                    } else {
-                        failedPathFindingPenalty += 10;
-                    }
+        if (this.attacker.getNavigator().getPath() == null || this.field_75445_i <= 0) {
+            this.entityPathEntity = this.attacker.getNavigator().getPathToEntityLiving(targetEntity);
+            this.field_75445_i = failedPathFindingPenalty + 4 + this.attacker.getRNG().nextInt(7);
+
+            if (this.attacker.getNavigator().getPath() != null) {
+                PathPoint finalPathPoint = this.attacker.getNavigator().getPath().getFinalPathPoint();
+
+                if (finalPathPoint != null && targetEntity.getDistanceSq(finalPathPoint.xCoord, finalPathPoint.yCoord, finalPathPoint.zCoord) < 1) {
+                    failedPathFindingPenalty = 0;
                 } else {
                     failedPathFindingPenalty += 10;
                 }
+            } else {
+                failedPathFindingPenalty += 10;
+            }
 
-                if (d0 > 1024.0D) {
-                    this.field_75445_i += 10;
-                } else if (d0 > 256.0D) {
-                    this.field_75445_i += 5;
-                }
+            double distanceSq = this.attacker.getDistanceSq(targetEntity.posX, targetEntity.boundingBox.minY, targetEntity.posZ);
 
-                if (!this.attacker.getNavigator()
-                    .tryMoveToEntityLiving(entitylivingbase, this.speedTowardsTarget)) {
-                    this.field_75445_i += 15;
-                }
+            if (distanceSq > 1024.0D) {
+                this.field_75445_i += 10;
+            } else if (distanceSq > 256.0D) {
+                this.field_75445_i += 5;
+            }
+
+            if (!this.attacker.getNavigator().tryMoveToEntityLiving(targetEntity, this.speedTowardsTarget)) {
+                this.field_75445_i += 15;
             }
         }
-        ci.cancel();
     }
+
 }
