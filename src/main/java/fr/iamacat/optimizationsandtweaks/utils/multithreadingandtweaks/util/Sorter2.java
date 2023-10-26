5f7538652bdec107f272c0f9f152b1df850ea6ff
QUENTIN453 <quentinlamauve@gmail.com>
2023-10-26 14:26:28 +0200

    Fix #42

--- src/main/java/fr/iamacat/optimizationsandtweaks/utils/multithreadingandtweaks/util/Sorter2.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/utils/multithreadingandtweaks/util/Sorter2.java
@@ -4,17 +4,24 @@ import net.minecraft.entity.Entity;
 
 import java.util.Comparator;
 
-public class Sorter2 implements Comparator<Entity> {
+public class Sorter2 implements Comparator {
     private final Entity theEntity;
 
     public Sorter2(Entity p_i1662_1) {
         this.theEntity = p_i1662_1;
     }
 
-    @Override
-    public int compare(Entity p_compare_1, Entity p_compare_2) {
-        double d0 = this.theEntity.getDistanceSqToEntity(p_compare_1);
-        double d1 = this.theEntity.getDistanceSqToEntity(p_compare_2);
+    public int compare(Entity p_compare_1_, Entity p_compare_2_)
+    {
+        double d0 = this.theEntity.getDistanceSqToEntity(p_compare_1_);
+        double d1 = this.theEntity.getDistanceSqToEntity(p_compare_2_);
         return Double.compare(d0, d1);
+    }
+    @Override
+    public int compare(Object o1, Object o2) {
+        Entity e1 = (Entity) o1;
+        Entity e2 = (Entity) o2;
+
+        return compare(e1, e2);
     }
 }
