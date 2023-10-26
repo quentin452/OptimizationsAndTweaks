e645723d394fc1040f26b98e8f123d1259d58ebf
QUENTIN453 <quentinlamauve@gmail.com>
2023-10-27 00:19:02 +0200

    Update MixinPathFinder.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/pathfinding/MixinPathFinder.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/pathfinding/MixinPathFinder.java
@@ -1,9 +1,5 @@
 package fr.iamacat.optimizationsandtweaks.mixins.common.core.pathfinding;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListMap;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
@@ -12,23 +8,23 @@ import net.minecraft.pathfinding.Path;
 import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.pathfinding.PathFinder;
 import net.minecraft.pathfinding.PathPoint;
-
+import net.minecraft.util.IntHashMap;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import fr.iamacat.optimizationsandtweaks.config.OptimizationsandTweaksConfig;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Mixin(PathFinder.class)
 public class MixinPathFinder {
     @Shadow
-    private PathPoint[] pathOptions = new PathPoint[32];
+    private boolean canEntityDrown;
 
+    @Shadow
+    private PathPoint[] pathOptions = new PathPoint[32];
     @Shadow
     private boolean isWoddenDoorAllowed;
     /** should the PathFinder disregard BlockMovement type materials in its path */
@@ -42,38 +38,48 @@ public class MixinPathFinder {
     @Shadow
     private Path path = new Path();
     @Unique
-    private ConcurrentSkipListMap<Integer, PathPoint> multithreadingandtweaks$pointMap = new ConcurrentSkipListMap<>();
+    float totalPathDistance;
+    /** The linear distance to the next point */
+    @Unique
+    float distanceToNext;
+    /** The distance to the target */
+    @Unique
+    float distanceToTarget;
+    @Unique
+    PathPoint previous;
+    @Shadow
+    private IntHashMap pointMap = new IntHashMap();
+
 
     /**
      * @author iamacatfr
      * @reason optimize func_82565_a
      */
-    @Inject(method = "func_82565_a", at = @At("HEAD"), cancellable = true)
-    private static int func_82565_a(Entity entity, int x, int y, int z, PathPoint pathPoint, boolean checkWater,
-        boolean avoidWater, boolean checkDoors, CallbackInfoReturnable<PathPoint> cir) {
-        if (OptimizationsandTweaksConfig.enableMixinPathFinding) {
+    @Overwrite
+    public static int func_82565_a(Entity p_82565_0_, int p_82565_1_, int p_82565_2_, int p_82565_3_, PathPoint p_82565_4_, boolean p_82565_5_,
+        boolean p_82565_6_, boolean p_82565_7_) {
             boolean isTrapdoorPresent = false;
 
-            int posX = (int) entity.posX;
-            int posY = (int) entity.posY;
-            int posZ = (int) entity.posZ;
+            int posX = (int) p_82565_0_.posX;
+            int posY = (int) p_82565_0_.posY;
+            int posZ = (int) p_82565_0_.posZ;
             Block block;
 
-            for (int i = x; i < x + pathPoint.xCoord; ++i) {
-                for (int j = y; j < y + pathPoint.yCoord; ++j) {
-                    for (int k = z; k < z + pathPoint.zCoord; ++k) {
-                        block = entity.worldObj.getBlock(i, j, k);
+            for (int i = p_82565_1_; i < p_82565_1_ + p_82565_4_.xCoord; ++i) {
+                for (int j = p_82565_2_; j < p_82565_2_ + p_82565_4_.yCoord; ++j) {
+                    for (int k = p_82565_3_; k <p_82565_3_ + p_82565_4_.zCoord; ++k) {
+                        block = p_82565_0_.worldObj.getBlock(i, j, k);
                         Material material = block.getMaterial();
 
                         if (material != Material.air) {
                             if (block == Blocks.trapdoor) {
                                 isTrapdoorPresent = true;
                             } else if (block != Blocks.flowing_water && block != Blocks.water) {
-                                if (!checkDoors && block == Blocks.wooden_door) {
+                                if (!p_82565_7_ && block == Blocks.wooden_door) {
                                     return 0;
                                 }
                             } else {
-                                if (checkWater) {
+                                if (p_82565_5_) {
                                     return -1;
                                 }
                                 isTrapdoorPresent = true;
@@ -82,14 +88,14 @@ public class MixinPathFinder {
                             int renderType = block.getRenderType();
 
                             if (renderType == 9) {
-                                if (entity.worldObj.getBlock(posX, posY, posZ)
+                                if (p_82565_0_.worldObj.getBlock(posX, posY, posZ)
                                     .getRenderType() != 9
-                                    && entity.worldObj.getBlock(posX, posY - 1, posZ)
+                                    && p_82565_0_.worldObj.getBlock(posX, posY - 1, posZ)
                                         .getRenderType() != 9) {
                                     return -3;
                                 }
-                            } else if (!block.getBlocksMovement(entity.worldObj, i, j, k)
-                                && (!avoidWater || block != Blocks.wooden_door)) {
+                            } else if (!block.getBlocksMovement(p_82565_0_.worldObj, i, j, k)
+                                && (!p_82565_6_ || block != Blocks.wooden_door)) {
                                     if (renderType == 11 || block == Blocks.fence_gate || renderType == 32) {
                                         return -3;
                                     }
@@ -102,7 +108,7 @@ public class MixinPathFinder {
                                         return 0;
                                     }
 
-                                    if (!entity.handleLavaMovement()) {
+                                    if (!p_82565_0_.handleLavaMovement()) {
                                         return -2;
                                     }
                                 }
@@ -112,129 +118,209 @@ public class MixinPathFinder {
             }
 
             return isTrapdoorPresent ? 2 : 1;
-        }
-        cir.setReturnValue(null);
-        cir.cancel();
-        return x;
     }
 
     /**
      * @author iamacatfr
      * @reason optimize openPoint
      */
-    @Inject(method = "openPoint", at = @At("HEAD"), cancellable = true)
-    private final PathPoint openPoint(int x, int y, int z, CallbackInfoReturnable<PathPoint> cir) {
-        if (OptimizationsandTweaksConfig.enableMixinPathFinding) {
-            int hash = PathPoint.makeHash(x, y, z);
-            PathPoint pathPoint = this.multithreadingandtweaks$pointMap.get(hash);
+    @Overwrite
+    private final PathPoint openPoint(int p_75854_1_, int p_75854_2_, int p_75854_3_)
+    {
+        int l = PathPoint.makeHash(p_75854_1_, p_75854_2_, p_75854_3_);
+        PathPoint pathpoint = (PathPoint)this.pointMap.lookup(l);
 
-            if (pathPoint == null) {
-                pathPoint = new PathPoint(x, y, z);
-                this.multithreadingandtweaks$pointMap.put(hash, pathPoint);
-            }
-            cir.setReturnValue(pathPoint);
-            return pathPoint;
+        if (pathpoint == null)
+        {
+            pathpoint = new PathPoint(p_75854_1_, p_75854_2_, p_75854_3_);
+            this.pointMap.addKey(l, pathpoint);
         }
-        cir.cancel();
-        return null;
-    }
 
-    @Redirect(
-        method = "addToPath",
-        at = @At(value = "FIELD", target = "Lnet/minecraft/pathfinding/Path;pointMap:Ljava/util/Map;"))
-    private Map<?, ?> redirectPointMap(PathEntity pathEntity) {
-        return multithreadingandtweaks$pointMap;
+        return pathpoint;
+    }
+    @Unique
+    private PathEntity multithreadingandtweaks$createEntityPath(PathPoint p_75853_1_, PathPoint p_75853_2_)
+    {
+        int i = 1;
+        PathPoint pathpoint2;
+
+        for (pathpoint2 = p_75853_2_; previous != null; pathpoint2 = previous)
+        {
+            ++i;
+        }
+
+        PathPoint[] apathpoint = new PathPoint[i];
+        pathpoint2 = p_75853_2_;
+        --i;
+
+        for (apathpoint[i] = p_75853_2_; previous != null; apathpoint[i] = pathpoint2)
+        {
+            pathpoint2 = previous;
+            --i;
+        }
+        return new PathEntity(apathpoint);
+        /*
+          int i = 1;
+        PathPoint pathpoint2;
+
+        for (pathpoint2 = p_75853_2_; pathpoint2.previous != null; pathpoint2 = pathpoint2.previous)
+        {
+            ++i;
+        }
+
+        PathPoint[] apathpoint = new PathPoint[i];
+        pathpoint2 = p_75853_2_;
+        --i;
+
+        for (apathpoint[i] = p_75853_2_; pathpoint2.previous != null; apathpoint[i] = pathpoint2)
+        {
+            pathpoint2 = pathpoint2.previous;
+            --i;
+        }
+
+        return new PathEntity2(apathpoint);
+         */
+    }
+    /**
+     * @author
+     * @reason
+     */
+    @Unique
+    private PathEntity addToPath(Entity p_75861_1_, PathPoint p_75861_2_, PathPoint p_75861_3_, PathPoint p_75861_4_, float p_75861_5_) {
+        totalPathDistance = 0.0F;
+        distanceToNext = p_75861_2_.distanceToSquared(p_75861_3_);
+        distanceToTarget = distanceToNext;
+        this.path.clearPath();
+        this.path.addPoint(p_75861_2_);
+        PathPoint pathpoint3 = p_75861_2_;
+
+        while (!this.path.isPathEmpty()) {
+            PathPoint pathpoint4 = this.path.dequeue();
+
+            if (pathpoint4.equals(p_75861_3_)) {
+                return this.multithreadingandtweaks$createEntityPath(p_75861_2_, p_75861_3_);
+            }
+
+            if (pathpoint4.distanceToSquared(p_75861_3_) < pathpoint3.distanceToSquared(p_75861_3_)) {
+                pathpoint3 = pathpoint4;
+            }
+
+            pathpoint4.isFirst = true;
+            int i = this.findPathOptions(p_75861_1_, pathpoint4, p_75861_4_, p_75861_3_, p_75861_5_);
+
+            for (int j = 0; j < i; ++j) {
+                PathPoint pathpoint5 = this.pathOptions[j];
+                float f1 = totalPathDistance + pathpoint4.distanceToSquared(pathpoint5);
+
+                if (!pathpoint5.isAssigned() || f1 < totalPathDistance) {
+                    previous = pathpoint4;
+                    totalPathDistance = f1;
+                    distanceToNext = pathpoint5.distanceToSquared(p_75861_3_);
+
+                    if (pathpoint5.isAssigned()) {
+                        this.path.changeDistance(pathpoint5, totalPathDistance + distanceToNext);
+                    } else {
+                        distanceToTarget = totalPathDistance + distanceToNext;
+                        this.path.addPoint(pathpoint5);
+                    }
+                }
+            }
+        }
+
+        if (pathpoint3 == p_75861_2_) {
+            return null;
+        } else {
+            return this.multithreadingandtweaks$createEntityPath(p_75861_2_, pathpoint3);
+        }
+    }
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    private PathPoint getSafePoint(Entity p_75858_1_, int p_75858_2_, int p_75858_3_, int p_75858_4_, PathPoint p_75858_5_, int p_75858_6_)
+    {
+        PathPoint pathpoint1 = null;
+        int i1 = this.getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_, p_75858_4_, p_75858_5_);
+
+        if (i1 == 2)
+        {
+            return this.openPoint(p_75858_2_, p_75858_3_, p_75858_4_);
+        }
+        else
+        {
+            if (i1 == 1)
+            {
+                pathpoint1 = this.openPoint(p_75858_2_, p_75858_3_, p_75858_4_);
+            }
+
+            if (pathpoint1 == null && p_75858_6_ > 0 && i1 != -3 && i1 != -4 && this.getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_, p_75858_5_) == 1)
+            {
+                pathpoint1 = this.openPoint(p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_);
+                p_75858_3_ += p_75858_6_;
+            }
+
+            if (pathpoint1 != null)
+            {
+                int j1 = 0;
+                int k1 = 0;
+
+                while (p_75858_3_ > 0)
+                {
+                    k1 = this.getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_ - 1, p_75858_4_, p_75858_5_);
+
+                    if (this.isPathingInWater && k1 == -1)
+                    {
+                        return null;
+                    }
+
+                    if (k1 != 1)
+                    {
+                        break;
+                    }
+
+                    if (j1++ >= p_75858_1_.getMaxSafePointTries())
+                    {
+                        return null;
+                    }
+
+                    --p_75858_3_;
+
+                    if (p_75858_3_ > 0)
+                    {
+                        pathpoint1 = this.openPoint(p_75858_2_, p_75858_3_, p_75858_4_);
+                    }
+                }
+
+                if (k1 == -2)
+                {
+                    return null;
+                }
+            }
+
+            return pathpoint1;
+        }
     }
 
     /**
      * @author
      * @reason
      */
-    @Inject(method = "getSafePoint", at = @At("HEAD"), cancellable = true)
-    private PathPoint getSafePoint(Entity p_75858_1_, int p_75858_2_, int p_75858_3_, int p_75858_4_, PathPoint p_75858_5_, int p_75858_6_, CallbackInfoReturnable<PathPoint> cir) {
-        int verticalOffset = this.multithreadingandtweaks$getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_, p_75858_4_, p_75858_5_, cir);
-
-        if (verticalOffset == 2) {
-            cir.cancel();
-            cir.setReturnValue(openPoint(p_75858_2_, p_75858_3_,p_75858_4_, cir));
-            return null;
-        }
-
-        PathPoint resultPoint = null;
-
-        if (verticalOffset == 1) {
-            resultPoint = openPoint(p_75858_2_, p_75858_3_, p_75858_4_, cir);
-        }
-
-        while (resultPoint == null && p_75858_6_ > 0 && verticalOffset != -3 && verticalOffset != -4) {
-            int yOffset = this.multithreadingandtweaks$getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_, p_75858_5_, cir);
-
-            if (yOffset == 1) {
-                resultPoint = openPoint(p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_, cir);
-                p_75858_3_ += p_75858_6_;
-            } else {
-                break;
-            }
-
-            p_75858_6_--;
-        }
-
-        if (resultPoint != null) {
-            int tries = 0;
-            int yOffset = 0;
-
-            while (p_75858_3_ > 0) {
-                yOffset = this.multithreadingandtweaks$getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_ - 1, p_75858_4_, p_75858_5_, cir);
-
-                if (isPathingInWater && yOffset == -1) {
-                    cir.cancel();
-                    cir.setReturnValue(null);
-                    return null;
-                }
-
-                if (yOffset != 1) {
-                    break;
-                }
-
-                if (tries++ >= p_75858_1_.getMaxSafePointTries()) {
-                    cir.cancel();
-                    cir.setReturnValue(null);
-                    return null;
-                }
-
-                p_75858_3_--;
-
-                if (p_75858_3_ > 0) {
-                    resultPoint = openPoint(p_75858_2_, p_75858_3_, p_75858_4_, cir);
-                }
-            }
-
-            if (yOffset == -2) {
-                cir.cancel();
-                cir.setReturnValue(null);
-                return null;
-            }
-        }
-
-        cir.setReturnValue(resultPoint);
-        return resultPoint;
-    }
-
-    @Inject(method = "findPathOptions", at = @At("HEAD"), cancellable = true)
-    private int findPathOptions(Entity p_75860_1_, PathPoint p_75860_2_, PathPoint p_75860_3_, PathPoint p_75860_4_, float p_75860_5_, CallbackInfoReturnable cir)
+    @Overwrite
+    private int findPathOptions(Entity p_75860_1_, PathPoint p_75860_2_, PathPoint p_75860_3_, PathPoint p_75860_4_, float p_75860_5_)
     {
         int i = 0;
         byte b0 = 0;
 
-        if (this.multithreadingandtweaks$getVerticalOffset(p_75860_1_, p_75860_2_.xCoord, p_75860_2_.yCoord + 1, p_75860_2_.zCoord, p_75860_3_,cir) == 1)
+        if (this.getVerticalOffset(p_75860_1_, p_75860_2_.xCoord, p_75860_2_.yCoord + 1, p_75860_2_.zCoord, p_75860_3_) == 1)
         {
             b0 = 1;
         }
 
-        PathPoint pathpoint3 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord, p_75860_2_.yCoord, p_75860_2_.zCoord + 1, p_75860_3_, b0,cir);
-        PathPoint pathpoint4 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord - 1, p_75860_2_.yCoord, p_75860_2_.zCoord, p_75860_3_, b0,cir);
-        PathPoint pathpoint5 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord + 1, p_75860_2_.yCoord, p_75860_2_.zCoord, p_75860_3_, b0,cir);
-        PathPoint pathpoint6 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord, p_75860_2_.yCoord, p_75860_2_.zCoord - 1, p_75860_3_, b0,cir);
+        PathPoint pathpoint3 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord, p_75860_2_.yCoord, p_75860_2_.zCoord + 1, p_75860_3_, b0);
+        PathPoint pathpoint4 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord - 1, p_75860_2_.yCoord, p_75860_2_.zCoord, p_75860_3_, b0);
+        PathPoint pathpoint5 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord + 1, p_75860_2_.yCoord, p_75860_2_.zCoord, p_75860_3_, b0);
+        PathPoint pathpoint6 = this.getSafePoint(p_75860_1_, p_75860_2_.xCoord, p_75860_2_.yCoord, p_75860_2_.zCoord - 1, p_75860_3_, b0);
 
         if (pathpoint3 != null && !pathpoint3.isFirst && pathpoint3.distanceTo(p_75860_4_) < p_75860_5_)
         {
@@ -255,17 +341,20 @@ public class MixinPathFinder {
         {
             this.pathOptions[i++] = pathpoint6;
         }
-        cir.setReturnValue(i);
+
         return i;
     }
 
     @Unique
     private final Map<Integer, Integer> multithreadingandtweaks$blockCache = new ConcurrentHashMap<>();
 
-    @Unique
-    public int multithreadingandtweaks$getVerticalOffset(Entity entity, int x, int y, int z, PathPoint pathPoint,
-        CallbackInfoReturnable<PathPoint> cir) {
-        int key = ((x & 0xFFF) << 20) | ((z & 0xFFF) << 8) | (y & 0xFF);
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public int getVerticalOffset(Entity p_75855_1_, int p_75855_2_, int p_75855_3_, int p_75855_4_, PathPoint p_75855_5_) {
+        int key = ((p_75855_2_ & 0xFFF) << 20) | ((p_75855_4_ & 0xFFF) << 8) | (p_75855_3_ & 0xFF);
 
         Integer cachedResult = multithreadingandtweaks$blockCache.get(key);
         if (cachedResult != null) {
@@ -273,15 +362,14 @@ public class MixinPathFinder {
         }
 
         int result = func_82565_a(
-            entity,
-            x,
-            y,
-            z,
-            pathPoint,
+            p_75855_1_,
+            p_75855_2_,
+            p_75855_3_,
+            p_75855_4_,
+            p_75855_5_,
             this.isPathingInWater,
             this.isMovementBlockAllowed,
-            this.isWoddenDoorAllowed,
-            cir);
+            this.isWoddenDoorAllowed);
 
         multithreadingandtweaks$blockCache.put(key, result);
 
