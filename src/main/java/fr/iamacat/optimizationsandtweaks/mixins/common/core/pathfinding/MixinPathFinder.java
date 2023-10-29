cf1e3decaf628521232907a51e16e8fa68bd5273
QUENTIN453 <quentinlamauve@gmail.com>
2023-10-29 06:41:35 +0100

    Update MixinPathFinder.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/pathfinding/MixinPathFinder.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/pathfinding/MixinPathFinder.java
@@ -204,61 +204,52 @@ public class MixinPathFinder {
      */
     @Overwrite
     private PathPoint getSafePoint(Entity p_75858_1_, int p_75858_2_, int p_75858_3_, int p_75858_4_, PathPoint p_75858_5_, int p_75858_6_)
-    {
+ {
         PathPoint pathpoint1 = null;
         int i1 = this.getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_, p_75858_4_, p_75858_5_);
 
-        if (i1 == 2)
-        {
+        if (i1 == 2) {
             return this.openPoint(p_75858_2_, p_75858_3_, p_75858_4_);
-        }
-        else
-        {
-            if (i1 == 1)
-            {
+        } else {
+            if (i1 == 1) {
                 pathpoint1 = this.openPoint(p_75858_2_, p_75858_3_, p_75858_4_);
             }
 
-            if (pathpoint1 == null && p_75858_6_ > 0 && i1 != -3 && i1 != -4 && this.getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_, p_75858_5_) == 1)
-            {
-                pathpoint1 = this.openPoint(p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_);
-                p_75858_3_ += p_75858_6_;
+            if (pathpoint1 == null && p_75858_6_> 0 && i1 != -3 && i1 != -4) {
+                int verticalOffset = this.getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_, p_75858_5_);
+                if (verticalOffset == 1) {
+                    pathpoint1 = this.openPoint(p_75858_2_, p_75858_3_ + p_75858_6_, p_75858_4_);
+                    p_75858_3_ += p_75858_6_;
+                }
             }
 
-            if (pathpoint1 != null)
-            {
+            if (pathpoint1 != null) {
                 int j1 = 0;
                 int k1 = 0;
 
-                while (p_75858_3_ > 0)
-                {
+                while (p_75858_3_ > 0) {
                     k1 = this.getVerticalOffset(p_75858_1_, p_75858_2_, p_75858_3_ - 1, p_75858_4_, p_75858_5_);
 
-                    if (this.isPathingInWater && k1 == -1)
-                    {
+                    if (this.isPathingInWater && k1 == -1) {
                         return null;
                     }
 
-                    if (k1 != 1)
-                    {
+                    if (k1 != 1) {
                         break;
                     }
 
-                    if (j1++ >= p_75858_1_.getMaxSafePointTries())
-                    {
+                    if (j1++ >= p_75858_1_.getMaxSafePointTries()) {
                         return null;
                     }
 
                     --p_75858_3_;
 
-                    if (p_75858_3_ > 0)
-                    {
+                    if (p_75858_3_ > 0) {
                         pathpoint1 = this.openPoint(p_75858_2_, p_75858_3_, p_75858_4_);
                     }
                 }
 
-                if (k1 == -2)
-                {
+                if (k1 == -2) {
                     return null;
                 }
             }
