7eedbd3ce0e74624518881fbbc4b1cb477732b66
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-11-24 02:10:11 +0100

    Update MixinEntityItem.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/entity/MixinEntityItem.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/entity/MixinEntityItem.java
@@ -1,7 +1,6 @@
 package fr.iamacat.optimizationsandtweaks.mixins.common.core.entity;
 
-import java.util.Iterator;
-
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
@@ -11,7 +10,6 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.item.ItemExpireEvent;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -19,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import fr.iamacat.optimizationsandtweaks.config.OptimizationsandTweaksConfig;
+import java.util.Objects;
 
 @Mixin(EntityItem.class)
 public abstract class MixinEntityItem extends Entity {
@@ -44,99 +42,84 @@ public abstract class MixinEntityItem extends Entity {
      */
     @Inject(method = "onUpdate", at = @At("HEAD"), remap = false, cancellable = true)
     public void onUpdate(CallbackInfo ci) {
-        if (OptimizationsandTweaksConfig.enableMixinEntityItem) {
-            ItemStack stack = this.getDataWatcher()
-                .getWatchableObjectItemStack(10);
-            if (stack != null && stack.getItem() != null) {
-                if (stack.getItem()
-                    .onEntityItemUpdate(entityItem)) {
-                    return;
-                }
+        ItemStack item = this.optimizationsAndTweaks$getEntityItem();
+        if (item == null) {
+            this.setDead();
+            ci.cancel();
+            return;
+        }
+
+        if (Objects.requireNonNull(item.getItem()).onEntityItemUpdate(entityItem)) {
+            return;
+        }
+
+        super.onUpdate();
+
+        if (this.delayBeforeCanPickup > 0) {
+            --this.delayBeforeCanPickup;
+        }
+
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
+        this.motionY -= 0.03999999910593033D;
+        this.noClip = this.func_145771_j(this.posX, (this.boundingBox.minY + this.boundingBox.maxY) / 2.0D, this.posZ);
+        this.moveEntity(this.motionX, this.motionY, this.motionZ);
+
+        boolean positionChanged = (int) this.prevPosX != (int) this.posX || (int) this.prevPosY != (int) this.posY || (int) this.prevPosZ != (int) this.posZ;
+
+        if (positionChanged || this.ticksExisted % 25 == 0) {
+            Block block = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ));
+            Material material = block.getMaterial();
+
+            if (material == Material.lava) {
+                this.motionY = 0.2D;
+                float randomMotion = this.rand.nextFloat() - this.rand.nextFloat();
+                this.motionX = randomMotion * 0.2F;
+                this.motionZ = randomMotion * 0.2F;
+                this.playSound("random.fizz", 0.4F, 2.0F + this.rand.nextFloat() * 0.4F);
             }
 
-            if (this.optimizationsAndTweaks$getEntityItem() == null) {
-                this.setDead();
-            } else {
-                super.onUpdate();
+            if (!this.worldObj.isRemote) {
+                this.optimizationsAndTweaks$searchForOtherItemsNearby(ci);
+            }
+        }
 
-                if (this.delayBeforeCanPickup > 0) {
-                    --this.delayBeforeCanPickup;
-                }
+        float slipperiness = 0.98F;
+        if (this.onGround) {
+            Block blockUnder = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ));
+            slipperiness = blockUnder.slipperiness * 0.98F;
+        }
 
-                this.prevPosX = this.posX;
-                this.prevPosY = this.posY;
-                this.prevPosZ = this.posZ;
-                this.motionY -= 0.03999999910593033D;
-                this.noClip = this
-                    .func_145771_j(this.posX, (this.boundingBox.minY + this.boundingBox.maxY) / 2.0D, this.posZ);
-                this.moveEntity(this.motionX, this.motionY, this.motionZ);
-                boolean flag = (int) this.prevPosX != (int) this.posX || (int) this.prevPosY != (int) this.posY
-                    || (int) this.prevPosZ != (int) this.posZ;
+        this.motionX *= slipperiness;
+        this.motionY *= 0.9800000190734863D;
+        this.motionZ *= slipperiness;
 
-                if (flag || this.ticksExisted % 25 == 0) {
-                    if (this.worldObj
-                        .getBlock(
-                            MathHelper.floor_double(this.posX),
-                            MathHelper.floor_double(this.posY),
-                            MathHelper.floor_double(this.posZ))
-                        .getMaterial() == Material.lava) {
-                        this.motionY = 0.20000000298023224D;
-                        this.motionX = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F;
-                        this.motionZ = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F;
-                        this.playSound("random.fizz", 0.4F, 2.0F + this.rand.nextFloat() * 0.4F);
-                    }
+        if (this.onGround) {
+            this.motionY *= -0.5D;
+        }
 
-                    if (!this.worldObj.isRemote) {
-                        this.optimizationsAndTweaks$searchForOtherItemsNearby(ci);
-                    }
-                }
+        ++this.age;
 
-                float f = 0.98F;
-
-                if (this.onGround) {
-                    f = this.worldObj.getBlock(
-                        MathHelper.floor_double(this.posX),
-                        MathHelper.floor_double(this.boundingBox.minY) - 1,
-                        MathHelper.floor_double(this.posZ)).slipperiness * 0.98F;
-                }
-
-                this.motionX *= f;
-                this.motionY *= 0.9800000190734863D;
-                this.motionZ *= f;
-
-                if (this.onGround) {
-                    this.motionY *= -0.5D;
-                }
-
-                ++this.age;
-
-                ItemStack item = getDataWatcher().getWatchableObjectItemStack(10);
-
-                if (item != null && !this.worldObj.isRemote) {
-                    if (this.age >= lifespan) {
-                        ItemExpireEvent event = new ItemExpireEvent(
-                            entityItem,
-                            (item.getItem() == null ? 6000
-                                : item.getItem()
-                                    .getEntityLifespan(item, worldObj)));
-                        if (MinecraftForge.EVENT_BUS.post(event)) {
-                            lifespan += event.extraLife;
-                        } else {
-                            this.setDead();
-                        }
-                    }
-
-                    if (item.stackSize <= 0) {
-                        this.setDead();
-                    }
-                }
-
-                if (item != null && item.stackSize <= 0) {
+        if (!this.worldObj.isRemote) {
+            if (this.age >= lifespan) {
+                ItemExpireEvent event = new ItemExpireEvent(entityItem, (item.getItem() == null ? 6000 : item.getItem().getEntityLifespan(item, worldObj)));
+                if (MinecraftForge.EVENT_BUS.post(event)) {
+                    lifespan += event.extraLife;
+                } else {
                     this.setDead();
                 }
             }
-            ci.cancel();
+
+            if (item.stackSize <= 0) {
+                this.setDead();
+            }
         }
+
+        if (item.stackSize <= 0) {
+            this.setDead();
+        }
+        ci.cancel();
     }
 
     /**
@@ -155,12 +138,9 @@ public abstract class MixinEntityItem extends Entity {
      */
     @Unique
     private void optimizationsAndTweaks$searchForOtherItemsNearby(CallbackInfo ci) {
-        Iterator iterator = this.worldObj
-            .getEntitiesWithinAABB(EntityItem.class, this.boundingBox.expand(0.5D, 0.0D, 0.5D))
-            .iterator();
-
-        while (iterator.hasNext()) {
-            EntityItem entityitem = (EntityItem) iterator.next();
+        for (Object o : this.worldObj
+            .getEntitiesWithinAABB(EntityItem.class, this.boundingBox.expand(0.5D, 0.0D, 0.5D))) {
+            EntityItem entityitem = (EntityItem) o;
             this.combineItems(entityitem, ci);
         }
     }
@@ -171,43 +151,29 @@ public abstract class MixinEntityItem extends Entity {
      */
     @Inject(method = "combineItems", at = @At("HEAD"), remap = false, cancellable = true)
     public boolean combineItems(EntityItem p_70289_1_, CallbackInfo ci) {
-        if (OptimizationsandTweaksConfig.enableMixinEntityItem) {
-            if (p_70289_1_ == entityItem) {
-                return false;
-            } else if (p_70289_1_.isEntityAlive() && this.isEntityAlive()) {
-                ItemStack itemstack = this.optimizationsAndTweaks$getEntityItem();
-                ItemStack itemstack1 = p_70289_1_.getEntityItem();
-
-                if (itemstack1.getItem() != itemstack.getItem()) {
-                    return false;
-                } else if (itemstack1.hasTagCompound() ^ itemstack.hasTagCompound()) {
-                    return false;
-                } else if (itemstack1.hasTagCompound() && !itemstack1.getTagCompound()
-                    .equals(itemstack.getTagCompound())) {
-                        return false;
-                    } else if (itemstack1.getItem() == null) {
-                        return false;
-                    } else if (itemstack1.getItem()
-                        .getHasSubtypes() && itemstack1.getItemDamage() != itemstack.getItemDamage()) {
-                            return false;
-                        } else if (itemstack1.stackSize < itemstack.stackSize) {
-                            return p_70289_1_.combineItems(entityItem);
-                        } else if (itemstack1.stackSize + itemstack.stackSize > itemstack1.getMaxStackSize()) {
-                            return false;
-                        } else {
-                            itemstack1.stackSize += itemstack.stackSize;
-                            p_70289_1_.delayBeforeCanPickup = Math
-                                .max(p_70289_1_.delayBeforeCanPickup, this.delayBeforeCanPickup);
-                            p_70289_1_.age = Math.min(p_70289_1_.age, this.age);
-                            p_70289_1_.setEntityItemStack(itemstack1);
-                            this.setDead();
-                            return true;
-                        }
-            } else {
-                return false;
-            }
+        if (p_70289_1_ == entityItem || !p_70289_1_.isEntityAlive() || !this.isEntityAlive()) {
+            return false;
         }
+
+        ItemStack itemstack = this.optimizationsAndTweaks$getEntityItem();
+        ItemStack itemstack1 = p_70289_1_.getEntityItem();
+
+        if (itemstack1.getItem() != itemstack.getItem() ||
+            (itemstack1.hasTagCompound() ^ itemstack.hasTagCompound()) ||
+            (itemstack1.hasTagCompound() && !itemstack1.getTagCompound().equals(itemstack.getTagCompound())) ||
+            itemstack1.getItem() == null ||
+            (itemstack1.getItem().getHasSubtypes() && itemstack1.getItemDamage() != itemstack.getItemDamage()) ||
+            itemstack1.stackSize < itemstack.stackSize ||
+            itemstack1.stackSize + itemstack.stackSize > itemstack1.getMaxStackSize()) {
+            return false;
+        }
+
+        itemstack1.stackSize += itemstack.stackSize;
+        p_70289_1_.delayBeforeCanPickup = Math.max(p_70289_1_.delayBeforeCanPickup, this.delayBeforeCanPickup);
+        p_70289_1_.age = Math.min(p_70289_1_.age, this.age);
+        p_70289_1_.setEntityItemStack(itemstack1);
+        this.setDead();
         ci.cancel();
-        return false;
+        return true;
     }
 }
