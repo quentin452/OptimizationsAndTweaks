ccc040e226553acd9b51c9cd8bc91eef34837bc6
QUENTIN453 <quentinlamauve@gmail.com>
2023-12-13 18:01:22 +0100

    Update MixinWorldGenCustomFlowers.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/thaumcraft/MixinWorldGenCustomFlowers.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/thaumcraft/MixinWorldGenCustomFlowers.java
@@ -23,15 +23,24 @@ public class MixinWorldGenCustomFlowers extends WorldGenerator {
     }
     @Override
     public boolean generate(World world, Random par2Random, int par3, int par4, int par5) {
-        for(int var6 = 0; var6 < 18; ++var6) {
+        int numPlaced = 0;
+
+        for (int var6 = 0; var6 < 18; ++var6) {
             int var7 = par3 + par2Random.nextInt(8) - par2Random.nextInt(8);
             int var8 = par4 + par2Random.nextInt(4) - par2Random.nextInt(4);
             int var9 = par5 + par2Random.nextInt(8) - par2Random.nextInt(8);
+
             if (world.isAirBlock(var7, var8, var9) && (world.getBlock(var7, var8 - 1, var9) == Blocks.grass || world.getBlock(var7, var8 - 1, var9) == Blocks.sand)) {
                 world.setBlock(var7, var8, var9, this.plantBlock, this.plantBlockMeta, 3);
+                numPlaced++;
+            }
+
+            if (numPlaced >= 5) {
+                break;
             }
         }
 
         return true;
     }
+
 }
