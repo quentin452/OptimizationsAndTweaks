5c516dd240e089079ab0b7eb17abae43fd532876
QUENTIN453 <quentinlamauve@gmail.com>
2023-12-13 17:50:52 +0100

    Update MixinWorldGenGreatwoodTrees.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/thaumcraft/MixinWorldGenGreatwoodTrees.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/thaumcraft/MixinWorldGenGreatwoodTrees.java
@@ -117,12 +117,12 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
      */
     @Overwrite
     public void generateLeafNodeList() {
-        this.height = (int)((double)this.heightLimit * this.heightAttenuation);
+        this.height = (int)(this.heightLimit * this.heightAttenuation);
         if (this.height >= this.heightLimit) {
             this.height = this.heightLimit - 1;
         }
 
-        int var1 = (int)(1.382 + Math.pow(this.leafDensity * (double)this.heightLimit / 13.0, 2.0));
+        int var1 = (int)(1.382 + Math.pow(this.leafDensity * this.heightLimit / 13.0, 2.0));
         if (var1 < 1) {
             var1 = 1;
         }
@@ -142,25 +142,22 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
             while(var6 >= 0) {
                 int var7 = 0;
                 float var8 = this.layerSize(var6);
-                if (var8 < 0.0F) {
-                    --var3;
-                    --var6;
-                } else {
-                    for(double var9 = 0.5; var7 < var1; ++var7) {
-                        double var11 = this.scaleWidth * (double)var8 * ((double)this.rand.nextFloat() + 0.328);
-                        double var13 = (double)this.rand.nextFloat() * 2.0 * Math.PI;
-                        int var15 = MathHelper.floor_double(var11 * Math.sin(var13) + (double)this.basePos[0] + var9);
-                        int var16 = MathHelper.floor_double(var11 * Math.cos(var13) + (double)this.basePos[2] + var9);
+                if (!(var8 < 0.0F)) {
+                    for (double var9 = 0.5; var7 < var1; ++var7) {
+                        double var11 = this.scaleWidth * var8 * ( this.rand.nextFloat() + 0.328);
+                        double var13 = this.rand.nextFloat() * 2.0 * Math.PI;
+                        int var15 = MathHelper.floor_double(var11 * Math.sin(var13) + this.basePos[0] + var9);
+                        int var16 = MathHelper.floor_double(var11 * Math.cos(var13) + this.basePos[2] + var9);
                         int[] var17 = new int[]{var15, var3, var16};
                         int[] var18 = new int[]{var15, var3 + this.leafDistanceLimit, var16};
                         if (this.checkBlockLine(var17, var18) == -1) {
                             int[] var19 = new int[]{this.basePos[0], this.basePos[1], this.basePos[2]};
-                            double var20 = Math.sqrt(Math.pow((double)Math.abs(this.basePos[0] - var17[0]), 2.0) + Math.pow((double)Math.abs(this.basePos[2] - var17[2]), 2.0));
+                            double var20 = Math.sqrt(Math.pow(Math.abs(this.basePos[0] - var17[0]), 2.0) + Math.pow(Math.abs(this.basePos[2] - var17[2]), 2.0));
                             double var22 = var20 * this.branchSlope;
-                            if ((double)var17[1] - var22 > (double)var5) {
+                            if (var17[1] - var22 > var5) {
                                 var19[1] = var5;
                             } else {
-                                var19[1] = (int)((double)var17[1] - var22);
+                                var19[1] = (int) (var17[1] - var22);
                             }
 
                             if (this.checkBlockLine(var19, var17) == -1) {
@@ -173,9 +170,9 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
                         }
                     }
 
-                    --var3;
-                    --var6;
                 }
+                --var3;
+                --var6;
             }
 
             this.leafNodes = new int[var4][4];
@@ -265,20 +262,24 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
             return true;
         }
     }
-    @Shadow
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
     float layerSize(int par1) {
-        if ((double)par1 < (double)((float)this.heightLimit) * 0.3) {
+        if (par1 < (this.heightLimit) * 0.3) {
             return -1.618F;
         } else {
-            float var2 = (float)this.heightLimit / 2.0F;
-            float var3 = (float)this.heightLimit / 2.0F - (float)par1;
+            float var2 = this.heightLimit / 2.0F;
+            float var3 = this.heightLimit / 2.0F - par1;
             float var4;
             if (var3 == 0.0F) {
                 var4 = var2;
             } else if (Math.abs(var3) >= var2) {
                 var4 = 0.0F;
             } else {
-                var4 = (float)Math.sqrt(Math.pow((double)Math.abs(var2), 2.0) - Math.pow((double)Math.abs(var3), 2.0));
+                var4 = (float)Math.sqrt(Math.pow(Math.abs(var2), 2.0) - Math.pow(Math.abs(var3), 2.0));
             }
 
             var4 *= 0.5F;
@@ -336,9 +337,13 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
     float leafSize(int par1) {
         return par1 >= 0 && par1 < this.leafDistanceLimit ? (par1 != 0 && par1 != this.leafDistanceLimit - 1 ? 3.0F : 2.0F) : -1.0F;
     }
-    @Shadow
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
     void genTreeLayer(int par1, int par2, int par3, float par4, byte par5, Block par6) {
-        int var7 = (int)((double)par4 + 0.618D);
+        int var7 = (int)(par4 + 0.618D);
         byte var8 = otherCoordPairs[par5];
         byte var9 = otherCoordPairs[par5 + 3];
         int[] var10 = new int[]{par1, par2, par3};
@@ -351,22 +356,20 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
             var13 = -var7;
 
             while(var13 <= var7) {
-                double var15 = Math.pow((double)Math.abs(var12) + 0.5D, 2.0D) + Math.pow((double)Math.abs(var13) + 0.5D, 2.0D);
-                if(var15 > (double)(par4 * par4)) {
-                    ++var13;
-                } else {
+                double var15 = Math.pow(Math.abs(var12) + 0.5D, 2.0D) + Math.pow(Math.abs(var13) + 0.5D, 2.0D);
+                if (!(var15 > (par4 * par4))) {
                     try {
                         var11[var9] = var10[var9] + var13;
                         Block block = this.worldObj.getBlock(var11[0], var11[1], var11[2]);
-                        if((block == Blocks.air || block == ConfigBlocks.blockMagicalLeaves) && (block == null || block.canBeReplacedByLeaves(this.worldObj, var11[0], var11[1], var11[2]))) {
+                        if ((block == Blocks.air || block == ConfigBlocks.blockMagicalLeaves) && (block == null || block.canBeReplacedByLeaves(this.worldObj, var11[0], var11[1], var11[2]))) {
                             this.setBlockAndNotifyAdequately(this.worldObj, var11[0], var11[1], var11[2], par6, 0);
                         }
                     } catch (Exception var17) {
                         ;
                     }
 
-                    ++var13;
                 }
+                ++var13;
             }
         }
 
@@ -387,11 +390,19 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
         }
 
     }
-    @Shadow
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
     boolean leafNodeNeedsBase(int par1) {
-        return (double)par1 >= (double)this.heightLimit * 0.2;
+        return par1 >= this.heightLimit * 0.2;
     }
-    @Shadow
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
     void placeBlockLine(int[] par1ArrayOfInteger, int[] par2ArrayOfInteger, Block par3) {
         int[] var4 = new int[]{0, 0, 0};
         byte var5 = 0;
@@ -420,9 +431,9 @@ public abstract class MixinWorldGenGreatwoodTrees extends WorldGenAbstractTree {
             int var15 = 0;
 
             for(int var16 = var4[var6] + var9; var15 != var16; var15 += var9) {
-                var14[var6] = MathHelper.floor_double((double)(par1ArrayOfInteger[var6] + var15) + 0.5);
-                var14[var7] = MathHelper.floor_double((double)par1ArrayOfInteger[var7] + (double)var15 * var10 + 0.5);
-                var14[var8] = MathHelper.floor_double((double)par1ArrayOfInteger[var8] + (double)var15 * var12 + 0.5);
+                var14[var6] = MathHelper.floor_double((par1ArrayOfInteger[var6] + var15) + 0.5);
+                var14[var7] = MathHelper.floor_double(par1ArrayOfInteger[var7] + var15 * var10 + 0.5);
+                var14[var8] = MathHelper.floor_double(par1ArrayOfInteger[var8] + var15 * var12 + 0.5);
                 byte var17 = 0;
                 int var18 = Math.abs(var14[0] - par1ArrayOfInteger[0]);
                 int var19 = Math.abs(var14[2] - par1ArrayOfInteger[2]);
