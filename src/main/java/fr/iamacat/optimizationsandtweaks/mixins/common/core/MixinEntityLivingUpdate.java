e328796ac0df115ce6fcfcd7cf10833a17dccd41
QUENTIN453 <quentinlamauve@gmail.com>
2023-12-15 20:31:18 +0100

    Update MixinEntityLivingUpdate.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityLivingUpdate.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityLivingUpdate.java
@@ -36,7 +36,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fr.iamacat.optimizationsandtweaks.config.OptimizationsandTweaksConfig;
 import fr.iamacat.optimizationsandtweaks.utils.apache.commons.math3.util.FastMath;
 
-@Mixin(value = EntityLivingBase.class, priority = 1100)
+@Mixin(value = EntityLivingBase.class, priority = 999)
 public abstract class MixinEntityLivingUpdate extends Entity {
 
     @Unique
@@ -383,5 +383,154 @@ public abstract class MixinEntityLivingUpdate extends Entity {
                 .filter(Entity::canBePushed)
                 .forEach(this::collideWithEntity);
         }
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void moveEntityWithHeading(float p_70612_1_, float p_70612_2_)
+    {
+        double d0;
+
+        if (this.isInWater() && (!(entityLivingBase instanceof EntityPlayer) || !((EntityPlayer)entityLivingBase).capabilities.isFlying))
+        {
+            d0 = this.posY;
+            this.moveFlying(p_70612_1_, p_70612_2_, this.isAIEnabled() ? 0.04F : 0.02F);
+            this.moveEntity(this.motionX, this.motionY, this.motionZ);
+            this.motionX *= 0.800000011920929D;
+            this.motionY *= 0.800000011920929D;
+            this.motionZ *= 0.800000011920929D;
+            this.motionY -= 0.02D;
+
+            if (this.isCollidedHorizontally && this.isOffsetPositionInLiquid(this.motionX, this.motionY + 0.6000000238418579D - this.posY + d0, this.motionZ))
+            {
+                this.motionY = 0.30000001192092896D;
+            }
+        }
+        else if (this.handleLavaMovement() && (!(entityLivingBase instanceof EntityPlayer) || !((EntityPlayer)entityLivingBase).capabilities.isFlying))
+        {
+            d0 = this.posY;
+            this.moveFlying(p_70612_1_, p_70612_2_, 0.02F);
+            this.moveEntity(this.motionX, this.motionY, this.motionZ);
+            this.motionX *= 0.5D;
+            this.motionY *= 0.5D;
+            this.motionZ *= 0.5D;
+            this.motionY -= 0.02D;
+
+            if (this.isCollidedHorizontally && this.isOffsetPositionInLiquid(this.motionX, this.motionY + 0.6000000238418579D - this.posY + d0, this.motionZ))
+            {
+                this.motionY = 0.30000001192092896D;
+            }
+        }
+        else
+        {
+            float f2 = 0.91F;
+
+            if (this.onGround)
+            {
+                f2 = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ)).slipperiness * 0.91F;
+            }
+
+            float f3 = 0.16277136F / (f2 * f2 * f2);
+            float f4;
+
+            if (this.onGround)
+            {
+                f4 = this.getAIMoveSpeed() * f3;
+            }
+            else
+            {
+                f4 = this.jumpMovementFactor;
+            }
+
+            this.moveFlying(p_70612_1_, p_70612_2_, f4);
+            f2 = 0.91F;
+
+            if (this.onGround)
+            {
+                f2 = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ)).slipperiness * 0.91F;
+            }
+
+            if (this.isOnLadder())
+            {
+                float f5 = 0.15F;
+
+                if (this.motionX < (double)(-f5))
+                {
+                    this.motionX = (-f5);
+                }
+
+                if (this.motionX > (double)f5)
+                {
+                    this.motionX = f5;
+                }
+
+                if (this.motionZ < (double)(-f5))
+                {
+                    this.motionZ = (-f5);
+                }
+
+                if (this.motionZ > (double)f5)
+                {
+                    this.motionZ = f5;
+                }
+
+                this.fallDistance = 0.0F;
+
+                if (this.motionY < -0.15D)
+                {
+                    this.motionY = -0.15D;
+                }
+
+                boolean flag = this.isSneaking() && entityLivingBase instanceof EntityPlayer;
+
+                if (flag && this.motionY < 0.0D)
+                {
+                    this.motionY = 0.0D;
+                }
+            }
+
+            this.moveEntity(this.motionX, this.motionY, this.motionZ);
+
+            if (this.isCollidedHorizontally && this.isOnLadder())
+            {
+                this.motionY = 0.2D;
+            }
+
+            if (this.worldObj.isRemote && (!this.worldObj.blockExists((int)this.posX, 0, (int)this.posZ) || !this.worldObj.getChunkFromBlockCoords((int)this.posX, (int)this.posZ).isChunkLoaded))
+            {
+                if (this.posY > 0.0D)
+                {
+                    this.motionY = -0.1D;
+                }
+                else
+                {
+                    this.motionY = 0.0D;
+                }
+            }
+            else
+            {
+                this.motionY -= 0.08D;
+            }
+
+            this.motionY *= 0.9800000190734863D;
+            this.motionX *= f2;
+            this.motionZ *= f2;
+        }
+
+        this.prevLimbSwingAmount = this.limbSwingAmount;
+        d0 = this.posX - this.prevPosX;
+        double d1 = this.posZ - this.prevPosZ;
+        float f6 = MathHelper.sqrt_double(d0 * d0 + d1 * d1) * 4.0F;
+
+        if (f6 > 1.0F)
+        {
+            f6 = 1.0F;
+        }
+
+        this.limbSwingAmount += (f6 - this.limbSwingAmount) * 0.4F;
+        this.limbSwing += this.limbSwingAmount;
     }
 }
