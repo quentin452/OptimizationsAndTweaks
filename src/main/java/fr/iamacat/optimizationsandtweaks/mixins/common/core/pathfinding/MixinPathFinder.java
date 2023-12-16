15102458970a71f4230abcc053d990c099fb5f96
QUENTIN453 <quentinlamauve@gmail.com>
2023-12-16 01:04:33 +0100

    Revert "Update MixinPathFinder.java"
    This reverts commit d6ff2537a0b0ca2b8915ac1bea842039cdf9cc21.

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/pathfinding/MixinPathFinder.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/pathfinding/MixinPathFinder.java
@@ -6,26 +6,14 @@ import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.pathfinding.PathFinder;
 import net.minecraft.pathfinding.PathPoint;
-import net.minecraft.util.IntHashMap;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(PathFinder.class)
 public class MixinPathFinder {
-
-    @Shadow
-    private boolean isMovementBlockAllowed;
-    @Shadow
-    private boolean isPathingInWater;
-    @Shadow
-    private boolean isWoddenDoorAllowed;
-
-    @Shadow
-    private IntHashMap pointMap = new IntHashMap();
 
     /**
      * @author
@@ -100,80 +88,4 @@ public class MixinPathFinder {
         }
         return hasTrapdoor ? 2 : 1;
     }
-
-    @Overwrite
-    private PathPoint getSafePoint(Entity entity, int x, int y, int z, PathPoint currentPoint, int yOffset) {
-        int verticalOffset = this.getVerticalOffset(entity, x, y, z, currentPoint);
-
-        if (verticalOffset == 2 || verticalOffset == 1) {
-            return this.openPoint(x, y, z);
-        }
-
-        PathPoint safePoint = null;
-
-        if (yOffset > 0 && verticalOffset != -3 && verticalOffset != -4) {
-            int newY = y + yOffset;
-            int yOffsetIndex = this.getVerticalOffset(entity, x, newY, z, currentPoint);
-
-            if (yOffsetIndex == 1) {
-                safePoint = this.openPoint(x, newY, z);
-                y = newY;
-            }
-        }
-
-        if (safePoint != null) {
-            int tries = 0;
-
-            int offset = 0;
-            while (y > 0) {
-                offset = this.getVerticalOffset(entity, x, y - 1, z, currentPoint);
-
-                if (this.isPathingInWater && offset == -1) {
-                    return null;
-                }
-
-                if (offset != 1) {
-                    break;
-                }
-
-                if (tries++ >= entity.getMaxSafePointTries()) {
-                    return null;
-                }
-
-                --y;
-
-                if (y > 0) {
-                    safePoint = this.openPoint(x, y, z);
-                }
-            }
-
-            if (offset == -2) {
-                return null;
-            }
-        }
-
-        return safePoint;
-    }
-    @Shadow
-    private final PathPoint openPoint(int p_75854_1_, int p_75854_2_, int p_75854_3_)
-    {
-        int l = PathPoint.makeHash(p_75854_1_, p_75854_2_, p_75854_3_);
-        PathPoint pathpoint = (PathPoint)this.pointMap.lookup(l);
-
-        if (pathpoint == null)
-        {
-            pathpoint = new PathPoint(p_75854_1_, p_75854_2_, p_75854_3_);
-            this.pointMap.addKey(l, pathpoint);
-        }
-
-        return pathpoint;
-    }
-
-    @Shadow
-    public int getVerticalOffset(Entity p_75855_1_, int p_75855_2_, int p_75855_3_, int p_75855_4_, PathPoint p_75855_5_)
-    {
-        return func_82565_a(p_75855_1_, p_75855_2_, p_75855_3_, p_75855_4_, p_75855_5_, this.isPathingInWater, this.isMovementBlockAllowed, this.isWoddenDoorAllowed);
-    }
-
-
 }
