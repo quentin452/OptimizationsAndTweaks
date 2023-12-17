79a1f6ad21c40a20ff2f83e476cd480d5915aefc
QUENTIN453 <quentinlamauve@gmail.com>
2023-12-17 20:58:00 +0100

    Update MixinEntity.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntity.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntity.java
@@ -1,22 +1,17 @@
 package fr.iamacat.optimizationsandtweaks.mixins.common.core;
 
-import fr.iamacat.optimizationsandtweaks.utils.optimizationsandtweaks.Classers;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.MathHelper;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
-import net.minecraftforge.fluids.IFluidBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(Entity.class)
-public class MixinEntity {
+public abstract class MixinEntity {
 
     @Unique
     private Entity entity;
@@ -49,8 +44,7 @@ public class MixinEntity {
     @Shadow
     public float width;
 
-    public MixinEntity(Entity entity) {
-        this.entity = entity;
+    public MixinEntity() {
         this.boundingBox = AxisAlignedBB.getBoundingBox(0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D);
     }
 
@@ -58,5 +52,39 @@ public class MixinEntity {
     public float getEyeHeight()
     {
         return 0.0F;
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Unique
+    private String optimizationsAndTweaks$cachedEntityName = null;
+
+    /**
+     * @author
+     * @reason
+     */
+    @Shadow
+    public abstract String getEntityString();
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public String getCommandSenderName() {
+        if (optimizationsAndTweaks$cachedEntityName != null) {
+            return optimizationsAndTweaks$cachedEntityName;
+        }
+
+        String entityName = getEntityString();
+        String translatedName = (entityName != null) ?
+            StatCollector.translateToLocal("entity." + entityName + ".name") :
+            StatCollector.translateToLocal("entity.generic.name");
+
+        optimizationsAndTweaks$cachedEntityName = translatedName;
+
+        return translatedName;
     }
 }
