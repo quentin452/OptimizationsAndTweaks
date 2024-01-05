f32a4a58b44cd6db146c076028f89900cc895db2
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-05 16:20:12 +0100

    Update MixinEntityHamster.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/hamsterific/MixinEntityHamster.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/hamsterific/MixinEntityHamster.java
@@ -1,13 +1,25 @@
 package fr.iamacat.optimizationsandtweaks.mixins.common.hamsterific;
 
+import com.google.common.collect.UnmodifiableIterator;
+import com.google.common.reflect.ClassPath;
 import es.razzleberri.hamsterrific.EntityHamster;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Mixin(EntityHamster.class)
 public abstract class MixinEntityHamster  extends EntityTameable {
+    @Shadow
+    private static List<String> hamsterColorList;
+
     protected MixinEntityHamster(World p_i1604_1_) {
         super(p_i1604_1_);
     }
@@ -44,5 +56,44 @@ public abstract class MixinEntityHamster  extends EntityTameable {
     @Overwrite(remap = false)
     public void setBallColor(int b) {
         this.dataWatcher.updateObject(21, b);
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite(remap = false)
+    private String getRandomHamsterColor() {
+        this.hamsterColorInitialize();
+        if (!hamsterColorList.isEmpty()) {
+            Collections.shuffle(hamsterColorList);
+            return hamsterColorList.get(0);
+        }
+        return "";
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite(remap = false)
+    private void hamsterColorInitialize() {
+        if (hamsterColorList == null) {
+            hamsterColorList = new ArrayList<>();
+
+            try {
+                Pattern p = Pattern.compile("assets/minecraft/(mob/hamster/hamster_.*)");
+
+                for (ClassPath.ResourceInfo i : ClassPath.from(this.getClass().getClassLoader()).getResources()) {
+                    Matcher m = p.matcher(i.getResourceName());
+                    if (m.matches()) {
+                        String s = m.group(1);
+                        hamsterColorList.add(s);
+                    }
+                }
+            } catch (Exception var6) {
+                var6.printStackTrace();
+            }
+        }
     }
 }
