5f5fa5da3d68fa4f639d0fa794469badbcf84d7f
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-06 18:15:08 +0100

    Update BlockGrass.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/utils/optimizationsandtweaks/minecraft/BlockGrass.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/utils/optimizationsandtweaks/minecraft/BlockGrass.java
@@ -11,8 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BlockGrass {
 
     public static void updateTick(World worldIn, int x, int y, int z, Random random, CallbackInfo ci) {
-        int lightValue = worldIn.getBlockLightValue(x, y + 1, z);
-        int lightOpacity = worldIn.getBlockLightOpacity(x, y + 1, z);
+        int aboveBlockY = y + 1;
+
+        int lightValue = worldIn.getBlockLightValue(x, aboveBlockY, z);
+        int lightOpacity = worldIn.getBlockLightOpacity(x, aboveBlockY, z);
 
         if (lightValue < 4 && lightOpacity > 2) {
             worldIn.setBlock(x, y, z, Blocks.dirt);
@@ -21,12 +23,13 @@ public class BlockGrass {
             for (int i1 = x; i1 < x + 16; i1++) {
                 for (int k1 = z; k1 < z + 16; k1++) {
                     int j1 = worldIn.getHeightValue(i1, k1);
+                    int blockLightValue = worldIn.getBlockLightValue(i1, aboveBlockY, k1);
+                    int blockLightOpacity = worldIn.getBlockLightOpacity(i1, aboveBlockY, k1);
 
                     Block block = worldIn.getBlock(i1, j1 + 1, k1);
-                    int blockLightValue = worldIn.getBlockLightValue(i1, j1 + 1, k1);
-                    int blockLightOpacity = worldIn.getBlockLightOpacity(i1, j1 + 1, k1);
+                    int blockMetadata = worldIn.getBlockMetadata(i1, j1, k1);
 
-                    if (block == Blocks.dirt && worldIn.getBlockMetadata(i1, j1, k1) == 0
+                    if (block == Blocks.dirt && blockMetadata == 0
                         && blockLightValue >= 4
                         && blockLightOpacity <= 2) {
                         worldIn.setBlock(i1, j1, k1, Blocks.grass);
