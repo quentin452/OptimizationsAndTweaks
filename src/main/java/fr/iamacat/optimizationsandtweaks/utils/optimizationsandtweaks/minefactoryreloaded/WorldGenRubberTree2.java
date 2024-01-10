85b586e9dfabcccf21590f274b88cb936f0889f7
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-10 01:24:22 +0100

    Update MixinFixRubberTreesCascadingWorldgenLag

--- src/main/java/fr/iamacat/optimizationsandtweaks/utils/optimizationsandtweaks/minefactoryreloaded/WorldGenRubberTree2.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/utils/optimizationsandtweaks/minefactoryreloaded/WorldGenRubberTree2.java
@@ -3,6 +3,7 @@ package fr.iamacat.optimizationsandtweaks.utils.optimizationsandtweaks.minefacto
 import net.minecraft.block.Block;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import powercrystals.minefactoryreloaded.setup.MFRThings;
 
 import java.util.ArrayList;
@@ -12,6 +13,8 @@ import java.util.Random;
 public class WorldGenRubberTree2 {
     public void generateLeaves(World world, Random rand, int x, int y, int z, int treeHeight) {
         List<ChunkCoordinates> leavesToPlace = new ArrayList<>();
+        Chunk chunk = world.getChunkFromBlockCoords(x, z);
+
         for (int yOffset = y - 3 + treeHeight; yOffset <= y + treeHeight; ++yOffset) {
             int var12 = yOffset - (y + treeHeight);
             int center = 1 - var12 / 2;
@@ -19,7 +22,7 @@ public class WorldGenRubberTree2 {
                 for (int zOffset = z - center; zOffset <= z + center; ++zOffset) {
                     int xPos = xOffset - x;
                     int zPos = zOffset - z;
-                    if ((xPos != center || zPos != center || rand.nextInt(2) != 0 && var12 != 0) && (canPlaceLeaf(rand))) {
+                    if ((xPos != center || zPos != center || rand.nextInt(2) != 0 && var12 != 0) && canPlaceLeaf(rand) && chunk.isChunkLoaded) {
                         leavesToPlace.add(new ChunkCoordinates(xOffset, yOffset, zOffset));
                     }
                 }
@@ -29,20 +32,25 @@ public class WorldGenRubberTree2 {
     }
 
     private void placeLeaves(World world, List<ChunkCoordinates> leavesToPlace) {
+        if (leavesToPlace.isEmpty()) {
+            return;
+        }
+
+        List<ChunkCoordinates> validLeavesToPlace = new ArrayList<>();
         for (ChunkCoordinates pos : leavesToPlace) {
-            world.setBlock(pos.posX, pos.posY, pos.posZ, MFRThings.rubberLeavesBlock);
-            notifyBlockUpdate(world, pos.posX, pos.posY, pos.posZ, 0);
+            if (world.blockExists(pos.posX, pos.posY, pos.posZ) && world.getBlock(pos.posX, pos.posY, pos.posZ).isAir(world, pos.posX, pos.posY, pos.posZ) && world.getChunkProvider().chunkExists(pos.posX >> 4, pos.posZ >> 4)) {
+                validLeavesToPlace.add(pos);
+            }
+        }
+
+        if (!validLeavesToPlace.isEmpty()) {
+            for (ChunkCoordinates pos : validLeavesToPlace) {
+                world.setBlock(pos.posX, pos.posY, pos.posZ, MFRThings.rubberLeavesBlock,0,2);
+            }
         }
     }
 
     private boolean canPlaceLeaf(Random rand) {
         return rand.nextFloat() < 0.9f;
-    }
-
-    private void notifyBlockUpdate(World world, int x, int y, int z, int meta) {
-        world.markBlockForUpdate(x, y, z);
-        if (!world.isRemote) {
-            world.notifyBlockChange(x, y, z, Block.getBlockById(meta));
-        }
     }
 }
