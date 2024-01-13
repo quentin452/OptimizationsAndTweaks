c88fa06a6a2facc6b14cf81d8bff58278085ab41
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-13 15:21:28 +0100

    Update MixinMathHelper.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinMathHelper.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinMathHelper.java
@@ -6,14 +6,19 @@ import net.minecraft.util.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 import java.util.Random;
 
 @Mixin(MathHelper.class)
 public class MixinMathHelper {
+
+    @Unique
+    private static float[] SIN_TABLE = optimizationsAndTweaks$initSinTable();
+    @Unique
+    private static final float optimizationsAndTweaks$PI = (float) Math.PI;
+
     /** A table of sin values computed from 0 (inclusive) to 2*pi (exclusive), with steps of 2*PI / 65536. */
-    @Shadow
-    private static float[] SIN_TABLE = new float[65536];
     /**
      * Though it looks like an array, this is really more like a mapping.  Key (index of this array) is the upper 5 bits
      * of the result of multiplying a 32-bit unsigned integer by the B(2, 5) De Bruijn sequence 0x077CB531.  Value
@@ -24,51 +29,54 @@ public class MixinMathHelper {
     @Shadow
     private static final int[] multiplyDeBruijnBitPosition;
 
+    @Unique
+    private static float[] optimizationsAndTweaks$initSinTable() {
+        float[] sinTable = new float[65536];
+        for (int var0 = 0; var0 < 65536; ++var0) {
+            sinTable[var0] = (float) StrictMath.sin(var0 * optimizationsAndTweaks$PI * 2.0D / 65536.0D);
+        }
+        return sinTable;
+    }
     /**
      * sin looked up in a table
      */
     @Overwrite
-    public static final float sin(float p_76126_0_)
-    {
-        return SIN_TABLE[(int)(p_76126_0_ * 10430.378F) & 65535];
+    public static float sin(float p_76126_0_) {
+        return SIN_TABLE[(int) (p_76126_0_ * 10430.378F) & 65535];
     }
 
     /**
      * cos looked up in the sin table with the appropriate offset
      */
     @Overwrite
-    public static final float cos(float p_76134_0_)
-    {
-        return SIN_TABLE[(int)(p_76134_0_ * 10430.378F + 16384.0F) & 65535];
+    public static float cos(float p_76134_0_) {
+        return SIN_TABLE[(int) (p_76134_0_ * 10430.378F + 16384.0F) & 65535];
     }
     /**
      * @author
      * @reason
      */
     @Overwrite
-    public static final float sqrt_float(float p_76129_0_)
-    {
-        return (float)Math.sqrt((double)p_76129_0_);
-    }
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    public static final float sqrt_double(double p_76133_0_)
-    {
-        return (float)Math.sqrt(p_76133_0_);
+    public static float sqrt_float(float p_76129_0_) {
+        return (float) StrictMath.sqrt(p_76129_0_);
     }
 
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public static float sqrt_double(double p_76133_0_) {
+        return (float) StrictMath.sqrt(p_76133_0_);
+    }
     /**
      * Returns the greatest integer less than or equal to the float argument
      */
     @Overwrite
-    public static int floor_float(float p_76141_0_)
-    {
-        int i = (int)p_76141_0_;
-        return p_76141_0_ < (float)i ? i - 1 : i;
+    public static int floor_float(float p_76141_0_) {
+        return (int) p_76141_0_;
     }
+
 
     /**
      * returns par0 cast as an int, and no greater than Integer.MAX_VALUE-1024
@@ -84,20 +92,16 @@ public class MixinMathHelper {
      * Returns the greatest integer less than or equal to the double argument
      */
     @Overwrite
-    public static int floor_double(double p_76128_0_)
-    {
-        int i = (int)p_76128_0_;
-        return p_76128_0_ < (double)i ? i - 1 : i;
+    public static int floor_double(double p_76128_0_) {
+        return (int) StrictMath.floor(p_76128_0_);
     }
 
     /**
      * Long version of floor_double
      */
     @Overwrite
-    public static long floor_double_long(double p_76124_0_)
-    {
-        long i = (long)p_76124_0_;
-        return p_76124_0_ < (double)i ? i - 1L : i;
+    public static long floor_double_long(double p_76124_0_) {
+        return (long) StrictMath.floor(p_76124_0_);
     }
     @Overwrite
     @SideOnly(Side.CLIENT)
@@ -105,12 +109,14 @@ public class MixinMathHelper {
     {
         return (int)(p_154353_0_ >= 0.0D ? p_154353_0_ : -p_154353_0_ + 1.0D);
     }
+    /**
+     * @author
+     * @reason
+     */
     @Overwrite
-    public static float abs(float p_76135_0_)
-    {
+    public static float abs(float p_76135_0_) {
         return p_76135_0_ >= 0.0F ? p_76135_0_ : -p_76135_0_;
     }
-
     /**
      * Returns the unsigned value of an int.
      */
@@ -123,13 +129,13 @@ public class MixinMathHelper {
     public static int ceiling_float_int(float p_76123_0_)
     {
         int i = (int)p_76123_0_;
-        return p_76123_0_ > (float)i ? i + 1 : i;
+        return p_76123_0_ > i ? i + 1 : i;
     }
     @Overwrite
     public static int ceiling_double_int(double p_76143_0_)
     {
         int i = (int)p_76143_0_;
-        return p_76143_0_ > (double)i ? i + 1 : i;
+        return p_76143_0_ > i ? i + 1 : i;
     }
 
     /**
@@ -139,7 +145,7 @@ public class MixinMathHelper {
     @Overwrite
     public static int clamp_int(int p_76125_0_, int p_76125_1_, int p_76125_2_)
     {
-        return p_76125_0_ < p_76125_1_ ? p_76125_1_ : (p_76125_0_ > p_76125_2_ ? p_76125_2_ : p_76125_0_);
+        return p_76125_0_ < p_76125_1_ ? p_76125_1_ : (Math.min(p_76125_0_, p_76125_2_));
     }
 
     /**
@@ -149,13 +155,21 @@ public class MixinMathHelper {
     @Overwrite
     public static float clamp_float(float p_76131_0_, float p_76131_1_, float p_76131_2_)
     {
-        return p_76131_0_ < p_76131_1_ ? p_76131_1_ : (p_76131_0_ > p_76131_2_ ? p_76131_2_ : p_76131_0_);
+        return p_76131_0_ < p_76131_1_ ? p_76131_1_ : (Math.min(p_76131_0_, p_76131_2_));
     }
+    /**
+     * @author
+     * @reason
+     */
     @Overwrite
     public static double clamp_double(double p_151237_0_, double p_151237_2_, double p_151237_4_)
     {
-        return p_151237_0_ < p_151237_2_ ? p_151237_2_ : (p_151237_0_ > p_151237_4_ ? p_151237_4_ : p_151237_0_);
+        return p_151237_0_ < p_151237_2_ ? p_151237_2_ : (Math.min(p_151237_0_, p_151237_4_));
     }
+    /**
+     * @author
+     * @reason
+     */
     @Overwrite
     public static double denormalizeClamp(double p_151238_0_, double p_151238_2_, double p_151238_4_)
     {
@@ -178,7 +192,7 @@ public class MixinMathHelper {
             p_76132_2_ = -p_76132_2_;
         }
 
-        return p_76132_0_ > p_76132_2_ ? p_76132_0_ : p_76132_2_;
+        return Math.max(p_76132_0_, p_76132_2_);
     }
 
     /**
@@ -198,33 +212,34 @@ public class MixinMathHelper {
     @SideOnly(Side.CLIENT)
     public static boolean stringNullOrLengthZero(String p_76139_0_)
     {
-        return p_76139_0_ == null || p_76139_0_.length() == 0;
+        return p_76139_0_ == null || p_76139_0_.isEmpty();
     }
-    @Overwrite
-    public static int getRandomIntegerInRange(Random p_76136_0_, int p_76136_1_, int p_76136_2_)
-    {
-        return p_76136_1_ >= p_76136_2_ ? p_76136_1_ : p_76136_0_.nextInt(p_76136_2_ - p_76136_1_ + 1) + p_76136_1_;
-    }
+
     @Overwrite
     public static float randomFloatClamp(Random p_151240_0_, float p_151240_1_, float p_151240_2_)
     {
         return p_151240_1_ >= p_151240_2_ ? p_151240_1_ : p_151240_0_.nextFloat() * (p_151240_2_ - p_151240_1_) + p_151240_1_;
     }
+
     @Overwrite
-    public static double getRandomDoubleInRange(Random p_82716_0_, double p_82716_1_, double p_82716_3_)
-    {
-        return p_82716_1_ >= p_82716_3_ ? p_82716_1_ : p_82716_0_.nextDouble() * (p_82716_3_ - p_82716_1_) + p_82716_1_;
+    public static int getRandomIntegerInRange(Random p_76136_0_, int p_76136_1_, int p_76136_2_) {
+        return p_76136_0_.nextInt(p_76136_2_ - p_76136_1_ + 1) + p_76136_1_;
     }
+
+    @Overwrite
+    public static double getRandomDoubleInRange(Random p_82716_0_, double p_82716_1_, double p_82716_3_) {
+        return p_82716_0_.nextDouble() * (p_82716_3_ - p_82716_1_) + p_82716_1_;
+    }
+    /**
+     * @author
+     * @reason
+     */
     @Overwrite
     public static double average(long[] p_76127_0_)
     {
         long i = 0L;
-        long[] along1 = p_76127_0_;
-        int j = p_76127_0_.length;
 
-        for (int k = 0; k < j; ++k)
-        {
-            long l = along1[k];
+        for (long l : p_76127_0_) {
             i += l;
         }
 
@@ -235,62 +250,38 @@ public class MixinMathHelper {
      * the angle is reduced to an angle between -180 and +180 by mod, and a 360 check
      */
     @Overwrite
-    public static float wrapAngleTo180_float(float p_76142_0_)
-    {
-        p_76142_0_ %= 360.0F;
-
-        if (p_76142_0_ >= 180.0F)
-        {
-            p_76142_0_ -= 360.0F;
+    public static float wrapAngleTo180_float(float angle) {
+        if (angle >= 180.0F) {
+            angle -= 360.0F;
+        } else if (angle < -180.0F) {
+            angle += 360.0F;
         }
-
-        if (p_76142_0_ < -180.0F)
-        {
-            p_76142_0_ += 360.0F;
-        }
-
-        return p_76142_0_;
+        return angle;
     }
 
     /**
      * the angle is reduced to an angle between -180 and +180 by mod, and a 360 check
      */
     @Overwrite
-    public static double wrapAngleTo180_double(double p_76138_0_)
-    {
-        p_76138_0_ %= 360.0D;
-
-        if (p_76138_0_ >= 180.0D)
-        {
-            p_76138_0_ -= 360.0D;
+    public static double wrapAngleTo180_double(double angle) {
+        if (angle >= 180.0D) {
+            angle -= 360.0D;
+        } else if (angle < -180.0D) {
+            angle += 360.0D;
         }
-
-        if (p_76138_0_ < -180.0D)
-        {
-            p_76138_0_ += 360.0D;
-        }
-
-        return p_76138_0_;
+        return angle;
     }
 
     /**
-     * parses the string as integer or returns the second parameter if it fails
+     * @author
+     * @reason
      */
     @Overwrite
-    public static int parseIntWithDefault(String p_82715_0_, int p_82715_1_)
-    {
-        int j = p_82715_1_;
-
-        try
-        {
-            j = Integer.parseInt(p_82715_0_);
+    public static int parseIntWithDefault(String input, int defaultValue) {
+        if (!input.isEmpty()) {
+        return Integer.parseInt(input);
         }
-        catch (Throwable throwable)
-        {
-            ;
-        }
-
-        return j;
+        return defaultValue;
     }
 
     /**
@@ -301,13 +292,9 @@ public class MixinMathHelper {
     {
         int k = p_82714_1_;
 
-        try
+        if (!p_82714_0_.isEmpty())
         {
-            k = Integer.parseInt(p_82714_0_);
-        }
-        catch (Throwable throwable)
-        {
-            ;
+        k = Integer.parseInt(p_82714_0_);
         }
 
         if (k < p_82714_2_)
@@ -317,38 +304,26 @@ public class MixinMathHelper {
 
         return k;
     }
-
     /**
      * parses the string as double or returns the second parameter if it fails.
      */
     @Overwrite
     public static double parseDoubleWithDefault(String p_82712_0_, double p_82712_1_)
     {
-        double d1 = p_82712_1_;
-
-        try
-        {
-            d1 = Double.parseDouble(p_82712_0_);
-        }
-        catch (Throwable throwable)
-        {
-            ;
-        }
-
-        return d1;
+        return Double.parseDouble(p_82712_0_);
     }
+    /**
+     * @author
+     * @reason
+     */
     @Overwrite
     public static double parseDoubleWithDefaultAndMax(String p_82713_0_, double p_82713_1_, double p_82713_3_)
     {
         double d2 = p_82713_1_;
 
-        try
+        if (!p_82713_0_.isEmpty())
         {
-            d2 = Double.parseDouble(p_82713_0_);
-        }
-        catch (Throwable throwable)
-        {
-            ;
+        d2 = Double.parseDouble(p_82713_0_);
         }
 
         if (d2 < p_82713_3_)
@@ -358,21 +333,25 @@ public class MixinMathHelper {
 
         return d2;
     }
-
     /**
-     * Returns the input value rounded up to the next highest power of two.
+     * @author
+     * @reason
      */
     @Overwrite
     @SideOnly(Side.CLIENT)
-    public static int roundUpToPowerOfTwo(int p_151236_0_)
-    {
-        int j = p_151236_0_ - 1;
-        j |= j >> 1;
-        j |= j >> 2;
-        j |= j >> 4;
-        j |= j >> 8;
-        j |= j >> 16;
-        return j + 1;
+    public static int roundUpToPowerOfTwo(int value) {
+        if (value <= 0)
+            return 0;
+
+        value--;
+
+        value |= value >> 1;
+        value |= value >> 2;
+        value |= value >> 4;
+        value |= value >> 8;
+        value |= value >> 16;
+
+        return value + 1;
     }
 
     /**
@@ -380,9 +359,8 @@ public class MixinMathHelper {
      */
     @Overwrite
     @SideOnly(Side.CLIENT)
-    private static boolean isPowerOfTwo(int p_151235_0_)
-    {
-        return p_151235_0_ != 0 && (p_151235_0_ & p_151235_0_ - 1) == 0;
+    private static boolean isPowerOfTwo(int value) {
+        return (value > 0) && ((value & (value - 1)) == 0);
     }
 
     /**
@@ -392,10 +370,9 @@ public class MixinMathHelper {
      */
     @Overwrite
     @SideOnly(Side.CLIENT)
-    private static int calculateLogBaseTwoDeBruijn(int p_151241_0_)
-    {
-        p_151241_0_ = isPowerOfTwo(p_151241_0_) ? p_151241_0_ : roundUpToPowerOfTwo(p_151241_0_);
-        return multiplyDeBruijnBitPosition[(int)((long)p_151241_0_ * 125613361L >> 27) & 31];
+    private static int calculateLogBaseTwoDeBruijn(int value) {
+        value = (value > 0) ? value : 1;
+        return multiplyDeBruijnBitPosition[(int)(value * 125613361L >> 27) & 31];
     }
 
     /**
@@ -404,37 +381,31 @@ public class MixinMathHelper {
      */
     @Overwrite
     @SideOnly(Side.CLIENT)
-    public static int calculateLogBaseTwo(int p_151239_0_)
-    {
-        return calculateLogBaseTwoDeBruijn(p_151239_0_) - (isPowerOfTwo(p_151239_0_) ? 0 : 1);
+    public static int calculateLogBaseTwo(int value) {
+        value = (value > 0) ? value : 1;
+
+        int log2 = multiplyDeBruijnBitPosition[(int)(value * 125613361L >> 27) & 31];
+
+        return (1 << log2) == value ? log2 : log2 - 1;
     }
+    /**
+     * @author
+     * @reason
+     */
     @Overwrite
     @SideOnly(Side.CLIENT)
-    public static int func_154354_b(int p_154354_0_, int p_154354_1_)
-    {
-        if (p_154354_1_ == 0)
-        {
-            return 0;
-        }
-        else
-        {
-            if (p_154354_0_ < 0)
-            {
-                p_154354_1_ *= -1;
-            }
-
+    public static int func_154354_b(int p_154354_0_, int p_154354_1_) {
+        if (p_154354_1_ != 0) {
             int k = p_154354_0_ % p_154354_1_;
-            return k == 0 ? p_154354_0_ : p_154354_0_ + p_154354_1_ - k;
+            return (k == 0) ? p_154354_0_ : p_154354_0_ + ((p_154354_0_ < 0) ? -p_154354_1_ : p_154354_1_) - k;
         }
+        return 0;
     }
-
-    static
-    {
-        for (int var0 = 0; var0 < 65536; ++var0)
-        {
-            SIN_TABLE[var0] = (float)Math.sin((double)var0 * Math.PI * 2.0D / 65536.0D);
+    static {
+        for (int var0 = 0; var0 < 65536; ++var0) {
+            SIN_TABLE[var0] = (float) StrictMath.sin(var0 * optimizationsAndTweaks$PI * 2.0D / 65536.0D);
         }
 
-        multiplyDeBruijnBitPosition = new int[] {0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9};
+        multiplyDeBruijnBitPosition = new int[]{0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9};
     }
 }
