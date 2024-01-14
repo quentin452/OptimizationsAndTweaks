742787bc3da9791b3f1bfa277b8149e0cae42f3b
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-14 13:58:48 +0100

    Revert "Update MixinEntityItem.java"
    This reverts commit a679ee94c8df3ea7df22974767a7b218c70cb7ea.

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/entity/MixinEntityItem.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/entity/MixinEntityItem.java
@@ -2,18 +2,13 @@ package fr.iamacat.optimizationsandtweaks.mixins.common.core.entity;
 
 import java.util.Iterator;
 
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.item.ItemExpireEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
@@ -91,91 +86,4 @@ public abstract class MixinEntityItem extends Entity {
             .getWatchableObjectItemStack(10);
         return itemstack == null ? new ItemStack(Blocks.stone) : itemstack;
     }
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    public void onUpdate() {
-        ItemStack stack = this.getDataWatcher().getWatchableObjectItemStack(10);
-
-        if (stack != null && stack.getItem() != null && stack.getItem().onEntityItemUpdate(entityItem)) {
-            return;
-        }
-
-        if (this.getEntityItem() == null) {
-            this.setDead();
-            return;
-        }
-
-        super.onUpdate();
-
-        if (this.delayBeforeCanPickup > 0) {
-            --this.delayBeforeCanPickup;
-        }
-
-        this.prevPosX = this.posX;
-        this.prevPosY = this.posY;
-        this.prevPosZ = this.posZ;
-        this.motionY -= 0.03999999910593033D;
-        this.noClip = this.func_145771_j(this.posX, (this.boundingBox.minY + this.boundingBox.maxY) / 2.0D, this.posZ);
-        this.moveEntity(this.motionX, this.motionY, this.motionZ);
-
-        boolean flag = (int) this.prevPosX != (int) this.posX || (int) this.prevPosY != (int) this.posY || (int) this.prevPosZ != (int) this.posZ;
-
-        if (flag || this.ticksExisted % 25 == 0) {
-            if (this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)).getMaterial() == Material.lava) {
-                this.optimizationsAndTweaks$handleLavaCollision();
-            }
-
-            if (!this.worldObj.isRemote) {
-                this.searchForOtherItemsNearby();
-            }
-        }
-
-        float f = this.onGround ? this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ)).slipperiness * 0.98F : 0.98F;
-
-        this.motionX *= f;
-        this.motionY *= 0.9800000190734863D;
-        this.motionZ *= f;
-
-        if (this.onGround) {
-            this.motionY *= -0.5D;
-        }
-
-        ++this.age;
-
-        ItemStack item = getDataWatcher().getWatchableObjectItemStack(10);
-
-        if (!this.worldObj.isRemote && this.age >= lifespan) {
-            optimizationsAndTweaks$handleItemExpiration(item);
-        }
-
-        if (item != null && item.stackSize <= 0) {
-            this.setDead();
-        }
-    }
-
-    @Unique
-    private void optimizationsAndTweaks$handleLavaCollision() {
-        this.motionY = 0.20000000298023224D;
-        this.motionX = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F;
-        this.motionZ = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F;
-        this.playSound("random.fizz", 0.4F, 2.0F + this.rand.nextFloat() * 0.4F);
-    }
-
-    @Unique
-    private void optimizationsAndTweaks$handleItemExpiration(ItemStack item) {
-        if (item != null) {
-            ItemExpireEvent event = new ItemExpireEvent(entityItem, item.getItem() == null ? 6000 : item.getItem().getEntityLifespan(item, worldObj));
-            if (MinecraftForge.EVENT_BUS.post(event)) {
-                lifespan += event.extraLife;
-            } else {
-                this.setDead();
-            }
-        } else {
-            this.setDead();
-        }
-    }
 }
-
