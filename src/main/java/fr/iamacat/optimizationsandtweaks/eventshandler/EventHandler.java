720be79e4eb3af5b7ce174eef34ce0f00f27d8a9
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-14 22:57:19 +0100

    Update EventHandler.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/eventshandler/EventHandler.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/eventshandler/EventHandler.java
@@ -1,13 +1,14 @@
 package fr.iamacat.optimizationsandtweaks.eventshandler;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.event.world.ChunkEvent;
-import org.spongepowered.asm.mixin.Unique;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,37 +16,12 @@ import java.util.List;
 import java.util.Set;
 
 public class EventHandler {
-
     // Remove all EntityItem during initial chunk generation.
     // to prevent lags caused by a large amount of EntityItem on mod packs at initial chunk loading.
     // inspired by Tidy Chunk mod from 1.12.2.
-    // todo remove all loggings/try and catch when i am sure that this method is work well
-    // todo fix he try to clear EntityItem every time i load chunk even when the chunk is old (potentially fixed)
-    private void optimizationsAndTweaks$clearEntityItemsAtChunkPopulatePost(Chunk chunk) {
-        World world = chunk.worldObj;
-
-        if (chunk.isTerrainPopulated) {
-            return;
-        }
-
-        List<EntityItem> entitiesToRemove = new ArrayList<>();
-        for (Object obj : world.loadedEntityList) {
-            if (obj instanceof EntityItem) {
-                EntityItem entityItem = (EntityItem) obj;
-                int entityX = MathHelper.floor_double(entityItem.posX);
-                int entityZ = MathHelper.floor_double(entityItem.posZ);
-
-                System.out.println("Checking EntityItem: " + entityItem + " at (" + entityX + ", " + entityZ + ")");
-                entitiesToRemove.add(entityItem);
-            }
-        }
-
-        for (EntityItem entity : entitiesToRemove) {
-            entity.setDead();
-            world.loadedEntityList.remove(entity);
-            System.out.println("EntityItem removed: " + entity);
-        }
-    }
+    // todo fix not all EntityItem don't get removed
+    // todo probably remove the set
+    private static final Set<Chunk> processedChunks = new HashSet<>();
 
     @SubscribeEvent
     public void onPopulateChunkPost(PopulateChunkEvent.Post event) {
@@ -53,8 +29,46 @@ public class EventHandler {
         int chunkX = event.chunkX;
         int chunkZ = event.chunkZ;
         Chunk chunk = world.getChunkFromChunkCoords(chunkX, chunkZ);
-        if(chunk != null) {
+        if (chunk != null) {
             optimizationsAndTweaks$clearEntityItemsAtChunkPopulatePost(chunk);
         }
     }
+    private void optimizationsAndTweaks$clearEntityItemsAtChunkPopulatePost(Chunk chunk) {
+        World world = chunk.worldObj;
+
+        if (!processedChunks.contains(chunk)) {
+            processedChunks.add(chunk);
+
+            List<EntityItem> entitiesToRemove = new ArrayList<>();
+
+            for (Object obj : world.loadedEntityList) {
+                if (obj instanceof EntityItem) {
+                    EntityItem entityItem = (EntityItem) obj;
+                    int entityX = MathHelper.floor_double(entityItem.posX);
+                    int entityZ = MathHelper.floor_double(entityItem.posZ);
+
+                    int chunkX = entityX >> 4;
+                    int chunkZ = entityZ >> 4;
+
+                    if (chunkX == chunk.xPosition && chunkZ == chunk.zPosition) {
+                        System.out.println("Checking EntityItem: " + entityItem + " at (" + entityX + ", " + entityZ + ")");
+                        entitiesToRemove.add(entityItem);
+                    }
+                }
+            }
+
+            for (EntityItem entity : entitiesToRemove) {
+                entity.setDead();
+                world.removeEntity(entity);
+            }
+        }
+    }
+
+  /*  @SubscribeEvent
+    public void onChunkLoad(ChunkEvent.Load event) {
+        Chunk chunk = event.getChunk();
+        optimizationsAndTweaks$clearEntityItemsAtChunkPopulatePost(chunk);
+    }
+
+   */
 }
