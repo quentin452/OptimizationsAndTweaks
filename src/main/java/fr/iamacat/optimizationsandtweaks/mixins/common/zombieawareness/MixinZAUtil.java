e1f5478700e4af4c831b28d897e267383e7cdf6a
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-16 01:29:02 +0100

    Update MixinZAUtil.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/zombieawareness/MixinZAUtil.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/zombieawareness/MixinZAUtil.java
@@ -19,7 +19,7 @@ public class MixinZAUtil {
     @Overwrite
     public static Entity getScent(Entity var0) {
         AxisAlignedBB searchBox = var0.boundingBox.expand(ZAConfig.maxPFRangeSense, ZAConfig.maxPFRangeSense, ZAConfig.maxPFRangeSense);
-        List<Entity> var1 = var0.worldObj.getEntitiesWithinAABBExcludingEntity(var0, searchBox, entity -> entity instanceof EntityScent);
+        List<Entity> var1 = var0.worldObj.getEntitiesWithinAABBExcludingEntity(var0, searchBox, EntityScent.class::isInstance);
         Entity var3 = null;
         for (Entity var2 : var1) {
             if (var0.getDistanceToEntity(var2) < ((EntityScent) var2).getRange() && var0.getDistanceToEntity(var2) > 5.0F && var0.worldObj.rand.nextInt(20) == 0) {
