b64c863c5001e556fc13bcc47d89f1814a482196
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-18 15:49:18 +0100

    Remove MixinNoiseGeneratorImproved

--- src/main/java/fr/iamacat/optimizationsandtweaks/config/OptimizationsandTweaksConfig.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/config/OptimizationsandTweaksConfig.java
@@ -278,11 +278,6 @@ public class OptimizationsandTweaksConfig {
     @Config.RequiresWorldRestart
     public static boolean enableMixinStatsComponent;
 
-    @Config.Comment("Optimize NoiseGeneratorImproved Class from Vanilla")
-    @Config.DefaultBoolean(true)
-    @Config.RequiresWorldRestart
-    public static boolean enableMixinNoiseGeneratorImproved;
-
     @Config.Comment("Optimize NoiseGeneratorOctaves Class from Vanilla")
     @Config.DefaultBoolean(true)
     @Config.RequiresWorldRestart
