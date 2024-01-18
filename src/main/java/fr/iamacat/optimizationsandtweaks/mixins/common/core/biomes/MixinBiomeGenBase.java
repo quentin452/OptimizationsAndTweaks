d9b6e856aa19f3343496d47db34da5a15c61dcd2
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-18 13:40:23 +0100

    Update MixinBiomeGenBase.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/biomes/MixinBiomeGenBase.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/biomes/MixinBiomeGenBase.java
@@ -102,6 +102,15 @@ public class MixinBiomeGenBase {
             }
         }
     }
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void genTerrainBlocks(World p_150573_1_, Random p_150573_2_, Block[] p_150573_3_, byte[] p_150573_4_, int p_150573_5_, int p_150573_6_, double p_150573_7_)
+    {
+        this.genBiomeTerrain(p_150573_1_, p_150573_2_, p_150573_3_, p_150573_4_, p_150573_5_, p_150573_6_, p_150573_7_);
+    }
 
     /**
      * @author
