5a88f8fe94013367e04ad90eec5213aaffb5a449
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-18 02:08:51 +0100

    Update MixinNoiseGeneratorImproved.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/noise/MixinNoiseGeneratorImproved.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/noise/MixinNoiseGeneratorImproved.java
@@ -73,17 +73,12 @@ public class MixinNoiseGeneratorImproved extends NoiseGenerator {
     public void optimizationsAndTweaks$populateNoiseArraysub1(double[] noiseArray, double xOffset, double zOffset,
                                                               int xSize, int zSize, double xScale, double zScale,
                                                               double densityScale) {
-        int index = 0;
         double densityScaleFactor = 1.0D / densityScale;
 
         for (int x = 0; x < xSize; ++x) {
             double xCoord = xOffset + x * xScale + this.xCoord;
             int intXCoord = (int) xCoord;
-
-            if (xCoord < intXCoord) {
-                --intXCoord;
-            }
-
+            intXCoord = (xCoord < intXCoord) ? intXCoord - 1 : intXCoord;
             int intX = intXCoord & 255;
             double dX = xCoord - intXCoord;
             double dX3 = dX * dX * dX;
@@ -93,33 +88,28 @@ public class MixinNoiseGeneratorImproved extends NoiseGenerator {
             for (int z = 0; z < zSize; ++z) {
                 double zCoord = zOffset + z * zScale + this.zCoord;
                 int intZCoord = (int) zCoord;
-
-                if (zCoord < intZCoord) {
-                    --intZCoord;
-                }
-
-                int intZ = intZCoord & 255;
+                intZCoord = (zCoord < intZCoord) ? intZCoord - 1 : intZCoord;
                 double dZ = zCoord - intZCoord;
                 double dZ3 = dZ * dZ * dZ;
                 double dZ6 = dZ3 * dZ3;
 
-                double d8 = 0.0D;
-                double d9 = 0.0D;
-                double d10 = 0.0D;
-                double d11 = 0.0D;
+                double d8 = 0.0D, d9 = 0.0D, d10 = 0.0D, d11 = 0.0D;
 
                 for (int l1 = 0; l1 < 4; ++l1) {
                     double d12 = (l1 & 2) - 1.0D;
                     double d13 = (l1 & 1) - 1.0D;
 
-                    d8 += grad(this.permutations[(intX + (int) d12) & 255], dX, dZ, dZ - d13);
-                    d9 += grad(this.permutations[(intX + 1 + (int) d12) & 255], dX - 1.0D, dZ, dZ - d13);
-                    d10 += grad(this.permutations[(intX + (int) d12) & 255], dX, dZ - 1.0D, dZ - d13);
-                    d11 += grad(this.permutations[(intX + 1 + (int) d12) & 255], dX - 1.0D, dZ - 1.0D, dZ - d13);
+                    int index1 = (intX + (int) d12) & 255;
+                    int index2 = (intX + 1 + (int) d12) & 255;
+
+                    d8 += grad(this.permutations[index1], dX, dZ, dZ - d13);
+                    d9 += grad(this.permutations[index2], dX - 1.0D, dZ, dZ - d13);
+                    d10 += grad(this.permutations[index1], dX, dZ - 1.0D, dZ - d13);
+                    d11 += grad(this.permutations[index2], dX - 1.0D, dZ - 1.0D, dZ - d13);
                 }
 
                 double d21 = d8 + dX9 * (d10 - d8) + dZ6 * (d11 - d8 + dX9 * (d9 - d8));
-                noiseArray[index++] += d21 * densityScaleFactor;
+                noiseArray[x * zSize + z] += d21 * densityScaleFactor;
             }
         }
     }
@@ -129,24 +119,13 @@ public class MixinNoiseGeneratorImproved extends NoiseGenerator {
     public void optimizationsAndTweaks$populateNoiseArraysub2(double[] noiseArray, double xOffset, double yOffset,
                                                               double zOffset, int xSize, int ySize, int zSize,
                                                               double xScale, double yScale, double zScale, double densityScale) {
-        double d9;
-        double d11;
-        double d8 = 0.0D;
-        d9 = 0.0D;
-        double d10 = 0.0D;
-        d11 = 0.0D;
-        int index = 0;
         double densityScaleFactor = 1.0D / densityScale;
         int lastIntY = -1;
 
         for (int x = 0; x < xSize; ++x) {
             double xCoord = xOffset + x * xScale + this.xCoord;
             int intXCoord = (int) xCoord;
-
-            if (xCoord < intXCoord) {
-                --intXCoord;
-            }
-
+            intXCoord = (xCoord < intXCoord) ? intXCoord - 1 : intXCoord;
             int intX = intXCoord & 255;
             double dX = xCoord - intXCoord;
             double dX3 = dX * dX * dX;
@@ -156,11 +135,7 @@ public class MixinNoiseGeneratorImproved extends NoiseGenerator {
             for (int z = 0; z < zSize; ++z) {
                 double zCoord = zOffset + z * zScale + this.zCoord;
                 int intZCoord = (int) zCoord;
-
-                if (zCoord < intZCoord) {
-                    --intZCoord;
-                }
-
+                intZCoord = (zCoord < intZCoord) ? intZCoord - 1 : intZCoord;
                 int intZ = intZCoord & 255;
                 double dZ = zCoord - intZCoord;
                 double dZ3 = dZ * dZ * dZ;
@@ -169,11 +144,7 @@ public class MixinNoiseGeneratorImproved extends NoiseGenerator {
                 for (int y = 0; y < ySize; ++y) {
                     double yCoord = yOffset + y * yScale + this.yCoord;
                     int intYCoord = (int) yCoord;
-
-                    if (yCoord < intYCoord) {
-                        --intYCoord;
-                    }
-
+                    intYCoord = (yCoord < intYCoord) ? intYCoord - 1 : intYCoord;
                     int intY = intYCoord & 255;
                     double dY = yCoord - intYCoord;
                     double dY3 = dY * dY * dY;
@@ -189,20 +160,20 @@ public class MixinNoiseGeneratorImproved extends NoiseGenerator {
                         int baseX4 = permutations[baseX3] + intZ;
                         int baseX5 = permutations[baseX3 + 1] + intZ;
 
-                        d8 = lerp(dX9, grad(permutations[baseX1], dX, dY, dZ),
+                        double d8 = lerp(dX9, grad(permutations[baseX1], dX, dY, dZ),
                             grad(permutations[baseX4], dX - 1.0D, dY, dZ));
-                        d9 = lerp(dX9, grad(permutations[baseX2], dX, dY - 1.0D, dZ),
+                        double d9 = lerp(dX9, grad(permutations[baseX2], dX, dY - 1.0D, dZ),
                             grad(permutations[baseX5], dX - 1.0D, dY - 1.0D, dZ));
-                        d10 = lerp(dX9, grad(permutations[baseX1 + 1], dX, dY, dZ - 1.0D),
+                        double d10 = lerp(dX9, grad(permutations[baseX1 + 1], dX, dY, dZ - 1.0D),
                             grad(permutations[baseX4 + 1], dX - 1.0D, dY, dZ - 1.0D));
-                        d11 = lerp(dX9, grad(permutations[baseX2 + 1], dX, dY - 1.0D, dZ - 1.0D),
+                        double d11 = lerp(dX9, grad(permutations[baseX2 + 1], dX, dY - 1.0D, dZ - 1.0D),
                             grad(permutations[baseX5 + 1], dX - 1.0D, dY - 1.0D, dZ - 1.0D));
-                    }
 
-                    double d18 = lerp(dY9, d8, d9);
-                    double d19 = lerp(dY9, d10, d11);
-                    double d20 = lerp(dZ6, d18, d19);
-                    noiseArray[index++] += d20 * densityScaleFactor;
+                        double d18 = lerp(dY9, d8, d9);
+                        double d19 = lerp(dY9, d10, d11);
+                        double d20 = lerp(dZ6, d18, d19);
+                        noiseArray[x * zSize * ySize + z * ySize + y] += d20 * densityScaleFactor;
+                    }
                 }
             }
         }
