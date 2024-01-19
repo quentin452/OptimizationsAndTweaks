785031feb6064f1e503a50065e2efaafd4d2699f
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-19 15:06:51 +0100

    MixinBlockGrass : Fix ArrayIndexOutOfBoundsException crash

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinBlockGrass.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinBlockGrass.java
@@ -14,6 +14,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static fr.iamacat.optimizationsandtweaks.utilsformods.vanilla.BlockGrass2.optimizationsAndTweaks$spreadGrass;
+
 @Mixin(BlockGrass.class)
 public class MixinBlockGrass {
 
@@ -43,33 +45,5 @@ public class MixinBlockGrass {
     @Unique
     private boolean optimizationsAndTweaks$canSpreadGrass(World worldIn, int x, int y, int z) {
         return worldIn.getBlockLightValue(x, y + 1, z) >= 9;
-    }
-
-    private void optimizationsAndTweaks$spreadGrass(World worldIn, int x, int y, int z, Random random) {
-        int chunkX = x >> 4;
-        int chunkZ = z >> 4;
-
-        Chunk chunk = worldIn.getChunkFromChunkCoords(chunkX, chunkZ);
-
-        if (!chunk.isChunkLoaded) {
-            return;
-        }
-
-        if (worldIn.getTotalWorldTime() % 10 == 0) {
-            for (int l = 0; l < 4; ++l) {
-                int i1 = x + random.nextInt(3) - 1;
-                int j1 = y + random.nextInt(5) - 3;
-                int k1 = z + random.nextInt(3) - 1;
-                Block block = chunk.getBlock(i1 & 15, j1, k1 & 15);
-                int metadata = chunk.getBlockMetadata(i1 & 15, j1, k1 & 15);
-
-                int lightValue = worldIn.getBlockLightValue(i1, j1 + 1, k1);
-                int lightOpacity = worldIn.getBlockLightOpacity(i1, j1 + 1, k1);
-
-                if (block == Blocks.dirt && metadata == 0 && lightValue >= 4 && lightOpacity <= 2) {
-                    worldIn.setBlock(i1, j1, k1, Blocks.grass, 0, 2);
-                }
-            }
-        }
     }
 }
