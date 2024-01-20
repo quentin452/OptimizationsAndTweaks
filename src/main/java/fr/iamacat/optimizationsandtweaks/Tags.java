b9001e48e6f8087f40823aeb0f67a7e26bdddb3a
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-20 22:42:20 +0100

    Update Tags.java

--- src/main/java/fr/iamacat/optimizationsandtweaks/Tags.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/Tags.java
@@ -5,9 +5,7 @@ public class Tags {
     public static final String MINENAUTICAMODID = "Minenautica";
     public static final String MODID = "optimizationsandtweaks";
     public static final String MODNAME = "Optimizations and Tweaks";
-    public static final String VERSION = "0.8.7.4.2";
     public static final String MCVERSION = "1.7.10";
-    public static final String GROUPNAME = "fr.iamacat.optimizationsandtweaks";
     public static final String CLIENTPROXY = "fr.iamacat.optimizationsandtweaks.proxy.ClientProxy";
     public static final String SERVERPROXY = "fr.iamacat.optimizationsandtweaks.proxy.CommonProxy";
 }
