2a54a3561193c482ca657eef7190e275e7f91d4c
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-10-10 00:19:30 +0200

    Reduced GenRainbowFlowers from LOT spawn chance

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/lotofthings/MixinLOTGeneration.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/lotofthings/MixinLOTGeneration.java
@@ -1,10 +1,19 @@
 package fr.iamacat.optimizationsandtweaks.mixins.common.lotofthings;
 
 import com.superdextor.LOT.LOTGeneration;
+import com.superdextor.LOT.init.LOTBlocks;
+import com.superdextor.LOT.worldgen.GenRainbowBox;
+import com.superdextor.LOT.worldgen.GenRainbowFlowers;
+
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.init.Blocks;
+import net.minecraft.block.Block;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,4 +32,22 @@ public class MixinLOTGeneration {
             return;
         }
     }
+
+    @Overwrite(remap = false)
+    public void generateOverworld(World world, Random rand, int x, int z) {
+      int xCh = x + rand.nextInt(16);
+      int yCh = rand.nextInt(100);
+      int zCh = z + rand.nextInt(16);
+      GenRainbowFlowers rainbowflower = new GenRainbowFlowers(1);
+      GenRainbowBox rainbowbox = new GenRainbowBox(Blocks.grass);
+      rainbowflower.func_76484_a(world, rand, xCh, yCh, zCh);
+      rainbowbox.func_76484_a(world, rand, xCh, yCh, zCh);
+      this.generateOre(LOTBlocks.copper_ore, world, rand, x, z, 4, 12, 24, 0, 89, Blocks.stone);
+      this.generateOre(LOTBlocks.ruby_ore, world, rand, x, z, 3, 8, 1, 0, 14, Blocks.stone);
+   }
+
+   @Shadow
+   public void generateOre(Block block, World world, Random random, int chunkX, int chunkZ, int minVienSize, int maxVienSize, int chance, int minY, int maxY, Block generateIn) {
+   
+   }
 }
