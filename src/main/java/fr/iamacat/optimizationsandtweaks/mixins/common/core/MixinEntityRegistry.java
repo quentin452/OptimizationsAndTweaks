6feb78f7d0409a08ccc22140f8fc73639cbf289d
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-10-15 05:52:40 +0200

    Revert "Fix EntityRegistry anti id conflict could not work if endlessids is not enabled"
    This reverts commit 13fcf23e176c0236709689b288bdec590afe47f8.

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityRegistry.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinEntityRegistry.java
@@ -1,17 +1,17 @@
 package fr.iamacat.optimizationsandtweaks.mixins.common.core;
 
-import java.util.BitSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.registry.EntityRegistry;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,21 +20,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.common.FMLLog;
 import org.apache.logging.log4j.Level;
-import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(EntityRegistry.class)
-public abstract class MixinEntityRegistry {
+public class MixinEntityRegistry {
 
     @Unique
     private static final AtomicInteger ENTITY_COUNTER = new AtomicInteger(0);
-
-    @Accessor("availableIndicies")
-    abstract BitSet getAvailableIndices();
-
-    @Unique
-    private static boolean isEndlessIDsLoaded() {
-        return Loader.isModLoaded("endlessids");
-    }
 
     @Redirect(
         method = "doModEntityRegistration",
@@ -86,50 +77,10 @@ public abstract class MixinEntityRegistry {
 
     @Unique
     private static int resolveEntityIdConflict(int id) {
-        if (isEndlessIDsLoaded()) {
-            // If EndlessIDs is loaded, use simple ID checking
-            int finalId = id;
-            if (EntityList.IDtoClassMapping.containsKey(finalId)) {
-                finalId = EntityRegistry.instance().findGlobalUniqueEntityId();
-                FMLLog.log(Level.WARN, "Entity ID %d is already taken, using ID %d instead (EndlessIDs mode)", id, finalId);
-            }
-            return finalId;
-        } else {
-            // If EndlessIDs is not loaded, use the original BitSet-based logic
-            try {
-                // Get the instance and check if the ID is available using the bitset
-                EntityRegistry instance = EntityRegistry.instance();
-                BitSet availableIndices = ((MixinEntityRegistry) (Object) instance).getAvailableIndices();
-                
-                // Apply the same ID adjustment logic as the original validateAndClaimId
-                int realId = adjustEntityId(id);
-                
-                if (availableIndices.get(realId)) {
-                    availableIndices.clear(realId);
-                    return realId;
-                } else {
-                    FMLLog.log(Level.WARN, "Entity ID %d (adjusted to %d) is already reserved, finding alternative", id, realId);
-                    return instance.findGlobalUniqueEntityId();
-                }
-            } catch (Exception e) {
-                FMLLog.log(Level.WARN, e, "Error during entity ID allocation, using fallback");
-                return EntityRegistry.instance().findGlobalUniqueEntityId();
-            }
+        int finalId = id;
+        if (EntityList.IDtoClassMapping.containsKey(finalId)) {
+            finalId = EntityRegistry.instance().findGlobalUniqueEntityId();
         }
-    }
-
-    @Unique
-    private static int adjustEntityId(int id) {
-        // Replicate the ID adjustment logic from validateAndClaimId
-        int realId = id;
-        if (id < Byte.MIN_VALUE) {
-            realId += 3000;
-        }
-
-        if (realId < 0) {
-            realId += Byte.MAX_VALUE;
-        }
-        
-        return realId;
+        return finalId;
     }
 }
