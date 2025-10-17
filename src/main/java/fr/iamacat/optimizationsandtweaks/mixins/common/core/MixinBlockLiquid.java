b02b25c51434978d8e09f6eb2a87a34217b00972
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-10-17 21:40:04 +0200

    Ensure BlockLiquid returns blue color on null biome

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinBlockLiquid.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinBlockLiquid.java
@@ -7,12 +7,21 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Mixin(BlockLiquid.class)
 public class MixinBlockLiquid extends Block {
+    @Unique
+    private static final Set<Integer> NULL_BIOME_DIMENSION_CACHE = new HashSet<>();
 
     protected MixinBlockLiquid(Material materialIn) {
         super(materialIn);
@@ -114,5 +123,29 @@ public class MixinBlockLiquid extends Block {
 
         return -1;
     }
-
+    /**
+     * Avoid returning an error if biome is null at getWaterColorMultiplier
+     */
+    @Redirect(
+        method = "colorMultiplier",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/biome/BiomeGenBase;getWaterColorMultiplier()I"
+        )
+    )
+    private int safeGetWaterColorMultiplier(BiomeGenBase biome, IBlockAccess world, int x, int y, int z) {
+        if (biome == null) {
+            if (world instanceof World) {
+                World actualWorld = (World) world;
+                int dimId = actualWorld.provider.dimensionId;
+                if (!NULL_BIOME_DIMENSION_CACHE.contains(dimId)) {
+                    System.err.println("[NullBiomeFix] BiomeGenBase is NULL in Dimension " + dimId + " (at x=" + x + ", z=" + z + ").");
+                    NULL_BIOME_DIMENSION_CACHE.add(dimId);
+                }
+            } 
+            return 0x4080FF; // blue color
+        }
+        
+        return biome.getWaterColorMultiplier();
+    }
 }
